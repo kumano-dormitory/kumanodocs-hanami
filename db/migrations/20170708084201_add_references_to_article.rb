b3615079b63b73dcac7df1cccdf02fd2aa043941
Yusuke Sangenya <longinus.eva@gmail.com>
2017-07-14 23:22:54 +0900

    hanami db applyを実行

--- db/migrations/20170708084201_add_references_to_article.rb
+++
@@ -1,8 +0,0 @@
-Hanami::Model.migration do
-  change do
-    alter_table :articles do
-      add_foreign_key :author_id, :authors, on_delete: :cascade, null: false
-      add_foreign_key :meeting_id, :meetings, on_delete: :cascade, null: false
-    end
-  end
-end
--- db/migrations/20170708084444_add_references_to_comment.rb
+++
@@ -1,8 +0,0 @@
-Hanami::Model.migration do
-  change do
-    alter_table :comments do
-      add_foreign_key :article_id, :articles, on_delete: :cascade, null: false
-      add_foreign_key :block_id, :blocks, on_delete: :cascade, null: false
-    end
-  end
-end
