de416e21b4901969fe8d4a0647a61a14ad889746
Yusuke Sangenya <longinus.eva@gmail.com>
2017-07-15 14:41:47 +0900

    インデント間違い

--- lib/kumanodocs_hanami/repositories/article_repository.rb
+++ lib/kumanodocs_hanami/repositories/article_repository.rb
@@ -1,7 +1,7 @@
-class ArticleRepository < Hanami::Repository
-    def by_meeting
-      articles.to_a
-        .group_by { |article| article.meeting_id }
-        .map { |meeting_id, articles| [MeetingRepository.new.find(meeting_id), articles] }
-    end
+class ArticleRepository < Hanami::Repository  
+  def by_meeting
+    articles.to_a
+      .group_by { |article| article.meeting_id }
+      .map { |meeting_id, articles| [MeetingRepository.new.find(meeting_id), articles] }
+  end
 end
