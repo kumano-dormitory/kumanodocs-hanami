dcaeb54845d3dddffe4e56df327c8c8be3ff2af9
Yusuke Sangenya <longinus.eva@gmail.com>
2017-08-02 15:26:15 +0900

    ArticleのNewを作成

---
+++ apps/web/controllers/article/new.rb
@@ -0,0 +1,16 @@
+module Web::Controllers::Article
+  class New
+    include Web::Action
+    expose :meetings, :categories
+
+    def initialize(meeting_repository: MeetingRepository.new, category_repository: CategoryRepository.new)
+      @meeting_repository = meeting_repository
+      @category_repository = category_repository
+    end
+
+    def call(params)
+      @meetings = @meeting_repository.in_time
+      @categories = @category_repository.all
+    end
+  end
+end
---
+++ spec/web/controllers/article/new_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require_relative '../../../../apps/web/controllers/article/new'
+
+describe Web::Controllers::Article::New do
+  let(:action) { Web::Controllers::Article::New.new }
+  let(:params) { Hash[] }
+
+  it 'is successful' do
+    response = action.call(params)
+    response[0].must_equal 200
+  end
+end
