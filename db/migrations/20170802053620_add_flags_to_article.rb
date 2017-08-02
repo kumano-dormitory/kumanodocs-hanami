a7c479df858ef38c1f2f991781349c52caa3aff6
junya shijubo <bo40.git@gmail.com>
2017-08-02 15:23:55 +0900

    前回のコミットで追加されていなかったので修正

--- db/migrations/20170802053620_add_flags_to_article.rb
+++ db/migrations/20170802053620_add_flags_to_article.rb
@@ -1,4 +1,8 @@
 Hanami::Model.migration do
   change do
+    alter_table :articles do
+      add_column :checked, :boolean, null: false, default: false
+      add_column :printed, :boolean, null: false, default: false
+    end
   end
 end
