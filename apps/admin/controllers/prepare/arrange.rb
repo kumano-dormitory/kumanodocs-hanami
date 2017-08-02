55c6c3c273e24c5a93416a4ae890925bae9228c4
junya shijubo <bo40.git@gmail.com>
2017-08-02 19:27:40 +0900

    prepare#arrangeのarticles取得の実装

--- apps/admin/controllers/prepare/arrange.rb
+++ apps/admin/controllers/prepare/arrange.rb
@@ -1,8 +1,14 @@
 module Admin::Controllers::Prepare
   class Arrange
     include Admin::Action
+    expose :articles
+
+    def initialize(article_repo: ArticleRepository.new)
+      @article_repo = article_repo
+    end
 
     def call(params)
+      @articles = @article_repo.by_meeting(params[:id])
     end
   end
 end
