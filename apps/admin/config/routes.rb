4f8c0456c49c9144ec16883e570a0c82e64945f1
junya shijubo <bo40.git@gmail.com>
2017-08-02 20:19:35 +0900

    ルーティングの修正。restfulに

--- apps/admin/config/routes.rb
+++ apps/admin/config/routes.rb
@@ -3,7 +3,7 @@
 #
 # Example:
 # get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
-get '/meeting/:id/articles/number/edit', to: 'number#edit'
-patch '/meeting/:id/articles/number/update', to: 'number#update'
-get '/meeting/:id/articles/status/edit', to: 'status#edit'
-patch '/meeting/:id/articles/number/update', to: 'status#update'
+get '/meeting/:id/articles/number/', to: 'number#edit'
+patch '/meeting/:id/articles/number/', to: 'number#update'
+get '/meeting/:id/articles/status/', to: 'status#edit'
+patch '/meeting/:id/articles/number/', to: 'status#update'
