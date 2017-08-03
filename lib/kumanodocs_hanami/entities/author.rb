8e2cc7f953491600a0e260105baae8d18c55d5b3
Yusuke Sangenya <longinus.eva@gmail.com>
2017-08-03 16:35:46 +0900

    パスワード暗号化のロジックをAuthorのクラスメソッドに移動した

--- lib/kumanodocs_hanami/entities/author.rb
+++ lib/kumanodocs_hanami/entities/author.rb
@@ -1,2 +1,6 @@
 class Author < Hanami::Entity
+  def self.crypt(password)
+    Digest::SHA256.hexdigest(password)
+  end
+
 end
--- lib/kumanodocs_hanami/repositories/author_repository.rb
+++ lib/kumanodocs_hanami/repositories/author_repository.rb
@@ -2,7 +2,7 @@ require 'digest'
 
 class AuthorRepository < Hanami::Repository
   def create_with_plain_password(name, password)
-    crypt_password = Digest::SHA256.hexdigest(password)
+    crypt_password = Author.crypt(password)
     create(name: name, crypt_password: crypt_password)
   end
 end
