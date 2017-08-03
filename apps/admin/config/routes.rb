6f5facddb7997f3e66451073648c7a3bb80c2c84
junya shijubo <bo40.git@gmail.com>
2017-08-03 09:07:41 +0900

    ルーティングに名前をつけてルーティングヘルパーが使えるように修正

--- apps/admin/config/routes.rb
+++ apps/admin/config/routes.rb
@@ -3,7 +3,7 @@
 #
 # Example:
 # get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
-get '/meeting/:id/articles/number/edit', to: 'article_number#edit'
-patch '/meeting/:id/articles/number/', to: 'article_number#update'
-get '/meeting/:id/articles/status/edit', to: 'article_status#edit'
-patch '/meeting/:id/articles/status/', to: 'article_status#update'
+get '/meeting/:id/articles/number/edit', to: 'article_number#edit', as: :edit_article_number
+patch '/meeting/:id/articles/number/', to: 'article_number#update', as: :update_article_number
+get '/meeting/:id/articles/status/edit', to: 'article_status#edit', as: :edit_article_status
+patch '/meeting/:id/articles/status/', to: 'article_status#update', as: :update_article_status
