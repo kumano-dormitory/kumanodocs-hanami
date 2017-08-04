8544986c2c5b74c077b8c995538dda268ad65a02
junya shijubo <bo40.git@gmail.com>
2017-08-05 02:35:57 +0900

    article_number#editアクションを実装し議案の並び替えページを表示できるように

--- apps/admin/controllers/article_number/edit.rb
+++ apps/admin/controllers/article_number/edit.rb
@@ -1,13 +1,17 @@
 module Admin::Controllers::ArticleNumber
   class Edit
     include Admin::Action
+    expose :meeting
     expose :articles
 
-    def initialize(article_repo: ArticleRepository.new)
+    def initialize(meeting_repo: MeetingRepository.new,
+                   article_repo: ArticleRepository.new)
+      @meeting_repo = meeting_repo
       @article_repo = article_repo
     end
 
     def call(params)
+      @meeting = @meeting_repo.find(params[:id])
       @articles = @article_repo.by_meeting(params[:id])
     end
   end
---
+++ apps/admin/views/article_number/form.rb
@@ -0,0 +1,32 @@
+module Admin::Views::ArticleNumber
+  module Form
+    def form(meeting, articles)
+      max = articles.count
+
+      form_for :meeting,
+               routes.update_article_number_path(id: meeting.id),
+               method: :patch do
+
+        articles.each_with_index do |article, idx|
+          value = article.number
+          div do
+            label article.title
+            div do
+              label 'article number'
+              number_field :article_number, min: 1,
+                                            max: max,
+                                            step: 1,
+                                            value: value,
+                                            name: "meeting[articles][][article_number]",
+                                            id: "meeting-articles-#{idx}-number"
+              hidden_field :article_id, name: "meeting[articles][][article_id]",
+                                        value: article.id
+            end
+          end
+        end
+
+        submit 'Send'
+      end
+    end
+  end
+end
