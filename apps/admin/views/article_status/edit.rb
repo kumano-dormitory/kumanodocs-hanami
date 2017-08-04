04415ef82485d2859353937c80f09f9fff01c00d
junya shijubo <bo40.git@gmail.com>
2017-08-04 20:33:44 +0900

    article_status#editアクションを表示できるように実装。

--- apps/admin/views/article_status/edit.rb
+++ apps/admin/views/article_status/edit.rb
@@ -1,5 +1,8 @@
+require_relative './form'
+
 module Admin::Views::ArticleStatus
   class Edit
     include Admin::View
+    include Form
   end
 end
---
+++ apps/admin/views/article_status/form.rb
@@ -0,0 +1,35 @@
+module Admin::Views::ArticleStatus
+  module Form
+    def form(meeting, articles)
+      form_for :meeting,
+               routes.update_article_status_path(id: meeting.id),
+               method: :patch do
+
+        articles.each_with_index do |article, idx|
+          div do
+            label article.title
+            div do
+              hidden_field :article_id, name: "meeting[articles][][article_id]", value: article.id
+              div do
+                check_box :checked, name: "meeting[articles][][checked]",
+                                    id: "meeting-articles-#{idx}-checked",
+                                    value: 'true',
+                                    checked: article.checked
+                label 'checked'
+              end
+              div do
+                check_box :printed, name: "meeting[articles][][printed]",
+                                    id: "meeting-articles-#{idx}-printed",
+                                    value: 'true',
+                                    checked: article.printed
+                label 'printed'
+              end
+            end
+          end
+        end
+
+        submit 'Send'
+      end
+    end
+  end
+end
