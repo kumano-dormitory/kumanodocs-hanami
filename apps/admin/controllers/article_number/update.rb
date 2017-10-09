a5b8899c34c951597c4397dee81568e6506d332a
bo40 <bo40.git@gmail.com>
2017-10-10 00:35:16 +0900

    article_numberの編集におけるvalidationを修正
    渡される順番の値が１から始まる並んだ整数値になっているかを判定するように修正した。

--- apps/admin/controllers/article_number/update.rb
+++ apps/admin/controllers/article_number/update.rb
@@ -1,13 +1,36 @@
+require 'hanami/validations'
+
 module Admin::Controllers::ArticleNumber
   class Update
     include Admin::Action
     expose :meeting
 
-    params do
-      required(:meeting).schema do
-        required(:articles) { array? { each { hash? }}}
+    params Class.new(Hanami::Action::Params) {
+      predicate(:int_values?, message: 'is not int values'){ |current|
+        !current['number'].nil? && !current['article_id'].nil? &&
+          current['number'].match(/\d+|/) && current['article_id'].match(/\d+|/)
+      }
+      predicate(:unique_array?, message: 'is not unique'){ |current|
+        return false unless current.instance_of?(Array)
+
+        numbers = current.map{ |item|
+          if item['number'].nil? || item['number'].eql?("")
+            0
+          else
+            item['number'].to_i
+          end
+        }.select{ |item| item.positive? } # select greater than 0
+        max = numbers.count
+        numbers.sort == (1..max).to_a
+      }
+
+      validations do
+        required(:meeting).schema do
+          required(:articles) { unique_array? { each { int_values? }}}
+        end
       end
-    end
+    }
+
 
     def initialize(meeting_repo: MeetingRepository.new,
                    article_repo: ArticleRepository.new)
