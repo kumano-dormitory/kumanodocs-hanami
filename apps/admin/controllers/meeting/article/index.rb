ec1fcbb11a41130c97a3edc8a7368ad8910b28ec
bo40 <bo40.git@gmail.com>
2017-10-11 03:15:07 +0900

    meeting#article#indexの実装を行った

--- apps/admin/controllers/meeting/article/index.rb
+++ apps/admin/controllers/meeting/article/index.rb
@@ -2,12 +2,14 @@ module Admin::Controllers::Meeting
   module Article
     class Index
       include Admin::Action
+      expose :articles
 
       def initialize(article_repo: ArticleRepository.new)
         @article_repo = article_repo
       end
-      
+
       def call(params)
+        @articles = @article_repo.by_meeting(params[:meeting_id])
       end
     end
   end
