4130a5cb7dee2bacf92095916d3ff683ba2c7806
bo40 <bo40.git@gmail.com>
2017-10-12 17:42:52 +0900

    meeting#article#showの実装をした

--- apps/admin/controllers/meeting/article/show.rb
+++ apps/admin/controllers/meeting/article/show.rb
@@ -2,12 +2,18 @@ module Admin::Controllers::Meeting
   module Article
     class Show
       include Admin::Action
+      expose :article
+      expose :meeting
 
-      def initialize(article_repo: ArticleRepository.new)
+      def initialize(article_repo: ArticleRepository.new,
+                     meeting_repo: MeetingRepository.new)
         @article_repo = article_repo
+        @meeting_repo = meeting_repo
       end
 
       def call(params)
+        @article = @article_repo.find_with_relations(params[:id])
+        @meeting = @meeting_repo.find(params[:meeting_id])
       end
     end
   end
