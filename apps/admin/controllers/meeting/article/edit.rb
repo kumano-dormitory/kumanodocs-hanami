11c209c394478cae35ddc35b3f32baa42107e4ea
bo40 <bo40.git@gmail.com>
2017-10-14 05:39:58 +0900

    article#editにおいてロックを取っていない場合はリダイレクトする処理を記述

--- apps/admin/controllers/meeting/article/edit.rb
+++ apps/admin/controllers/meeting/article/edit.rb
@@ -13,9 +13,13 @@ module Admin::Controllers::Meeting
       end
 
       def call(params)
-        @meetings = @meeting_repo.in_time
-        @categories = @category_repo.all
         @article = @article_repo.find_with_relations(params[:id])
+        if @article.author.locked?  && @article.author.lock_key == cookies[:article_lock_key]
+          @meetings = @meeting_repo.in_time
+          @categories = @category_repo.all
+        else
+          redirect_to routes.new_meeting_article_lock_path(meeting_id: params[:meeting_id], article_id: @article.id)
+        end
       end
     end
   end
