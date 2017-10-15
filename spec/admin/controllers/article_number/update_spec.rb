f2e00d18503e88e0322409c4ed4c2a0d45927d2c
bo40 <bo40.git@gmail.com>
2017-10-16 02:50:32 +0900

    article_number#updateのテストを実装した

--- spec/admin/controllers/article_number/update_spec.rb
+++ spec/admin/controllers/article_number/update_spec.rb
@@ -3,10 +3,31 @@ require_relative '../../../../apps/admin/controllers/article_number/update'
 
 describe Admin::Controllers::ArticleNumber::Update do
   let(:action) { Admin::Controllers::ArticleNumber::Update.new }
-  let(:params) { Hash[] }
+  let(:article_repo) { ArticleRepository.new }
+  let(:articles) { create_list(:article, 5) }
+  let(:meeting) { create(:meeting) }
 
   it 'is successful' do
+    numbers = ["1", "2", "3"].shuffle
+    articles.each { |article|
+      article_repo.update(article.id, meeting_id: meeting.id)
+    }
+    article_params = articles.sample(3).map.with_index { |article, index| {'article_id' => "#{article.id}", 'number' => numbers[index]} }
+    params = {id: meeting.id, meeting: {articles: article_params} }
+
     response = action.call(params)
-    response[0].must_equal 200
+    response[0].must_equal 302
+  end
+
+  it 'is rejected' do
+    numbers = ["1", "1", "3"].shuffle
+    articles.each { |article|
+      article_repo.update(article.id, meeting_id: meeting.id)
+    }
+    article_params = articles.sample(3).map.with_index { |article, index| {'article_id' => "#{article.id}", 'number' => numbers[index]} }
+    params = {id: meeting.id, meeting: {articles: article_params} }
+
+    response = action.call(params)
+    response[0].must_equal 422
   end
 end
