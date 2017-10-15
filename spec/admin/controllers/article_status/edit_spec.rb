2ac1fad0d562d4db0ac662b259a7938584271ef8
bo40 <bo40.git@gmail.com>
2017-10-16 01:07:26 +0900

    article_status#editのテストを修正

--- spec/admin/controllers/article_status/edit_spec.rb
+++ spec/admin/controllers/article_status/edit_spec.rb
@@ -3,10 +3,15 @@ require_relative '../../../../apps/admin/controllers/article_status/edit'
 
 describe Admin::Controllers::ArticleStatus::Edit do
   let(:action) { Admin::Controllers::ArticleStatus::Edit.new }
-  let(:params) { Hash[] }
+  let(:meeting_repo) { MeetingRepository.new }
+  let(:article) { create(:article) }
 
   it 'is successful' do
-    response = action.call(params)
+    _meeting = meeting_repo.find_with_articles(article.meeting_id)
+
+    response = action.call(id: article.meeting_id)
     response[0].must_equal 200
+
+    action.meeting.must_equal _meeting
   end
 end
