72c492f17664ea1ab4a59c0511bf6846f8c661a7
bo40 <bo40.git@gmail.com>
2017-10-16 08:57:30 +0900

    adminのarticle#showアクションのテストを実装した

--- spec/admin/controllers/meeting/article/show_spec.rb
+++ spec/admin/controllers/meeting/article/show_spec.rb
@@ -3,10 +3,16 @@ require_relative '../../../../../apps/admin/controllers/meeting/article/show'
 
 describe Admin::Controllers::Meeting::Article::Show do
   let(:action) { Admin::Controllers::Meeting::Article::Show.new }
-  let(:params) { Hash[] }
+  let(:meeting_repo) { MeetingRepository.new }
+  let(:article) { create(:article) }
+  let(:meeting) { meeting_repo.find(article.meeting_id) }
+  let(:params) { {meeting_id: article.meeting_id, id: article.id} }
 
   it 'is successful' do
     response = action.call(params)
     response[0].must_equal 200
+
+    action.meeting.must_equal meeting
+    action.article.must_equal article
   end
 end
