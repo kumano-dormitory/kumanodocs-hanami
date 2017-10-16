a3f02208b477ee4d9f21b5aa3736dea927989d0a
bo40 <bo40.git@gmail.com>
2017-10-16 20:52:39 +0900

    articleが削除されていることを確認するように修正

--- spec/web/controllers/article/destroy_spec.rb
+++ spec/web/controllers/article/destroy_spec.rb
@@ -3,11 +3,13 @@ require_relative '../../../../apps/web/controllers/article/destroy'
 
 describe Web::Controllers::Article::Destroy do
   let(:action) { Web::Controllers::Article::Destroy.new }
-  let(:article) { create(:article) }
-  let(:params) { {id: article.id} }
+  let(:article_repo) { ArticleRepository.new }
 
   it 'is successful' do
-    response = action.call(params)
+    article = create(:article)
+    pre_article_count = article_repo.all.count
+    response = action.call({id: article.id})
     response[0].must_equal 302
+    article_repo.all.count.must_equal pre_article_count - 1
   end
 end
