93a4a7c6c9c550602cf89a6c2034b01df2e730a7
bo40 <bo40.git@gmail.com>
2017-10-16 09:05:10 +0900

    adminのarticle#lock#newアクションのテストを実装した

--- spec/admin/controllers/meeting/article/lock/new_spec.rb
+++ spec/admin/controllers/meeting/article/lock/new_spec.rb
@@ -3,10 +3,23 @@ require_relative '../../../../../../apps/admin/controllers/meeting/article/lock/
 
 describe Admin::Controllers::Meeting::Article::Lock::New do
   let(:action) { Admin::Controllers::Meeting::Article::Lock::New.new }
-  let(:params) { Hash[] }
+  let(:author_repo) { AuthorRepository.new }
+  let(:article) { create(:article) }
+  let(:params) { {meeting_id: article.meeting_id, article_id: article.id} }
 
-  it 'is successful' do
+  it 'is successful (locked)' do
+    author_repo.lock(article.author_id, '')
     response = action.call(params)
     response[0].must_equal 200
+
+    action.locked.must_equal true
+  end
+
+  it 'is successful (unlocked)' do
+    author_repo.release_lock(article.author_id)
+    response = action.call(params)
+    response[0].must_equal 200
+
+    action.locked.must_equal false
   end
 end
