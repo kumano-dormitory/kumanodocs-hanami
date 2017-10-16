8f5a6f99df4ef60900cbbb756d8ee3e13884cfc5
bo40 <bo40.git@gmail.com>
2017-10-16 09:01:36 +0900

    adminのarticle#editアクションのテストを実装した

--- spec/admin/controllers/meeting/article/edit_spec.rb
+++ spec/admin/controllers/meeting/article/edit_spec.rb
@@ -3,10 +3,19 @@ require_relative '../../../../../apps/admin/controllers/meeting/article/edit'
 
 describe Admin::Controllers::Meeting::Article::Edit do
   let(:action) { Admin::Controllers::Meeting::Article::Edit.new }
-  let(:params) { Hash[] }
+  let(:author_repo) { AuthorRepository.new }
+  let(:article) { create(:article) }
+  let(:params) { {meeting_id: article.meeting_id, id: article.id} }
 
   it 'is successful' do
+    lock_key = author_repo.lock(article.author_id, '')
+    params.merge!("HTTP_COOKIE"=>"article_lock_key=#{lock_key}")
     response = action.call(params)
     response[0].must_equal 200
+  end
+
+  it 'is redirected' do
+    response = action.call(params)
+    response[0].must_equal 302
   end
 end
