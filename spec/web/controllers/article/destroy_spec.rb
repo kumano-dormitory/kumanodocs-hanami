7b1bd37629a3260033de97e0812f1eb4ec1883d3
bo40 <bo40.git@gmail.com>
2017-10-16 09:19:57 +0900

    article#destroyアクションのテストが通るように修正した

--- spec/web/controllers/article/destroy_spec.rb
+++ spec/web/controllers/article/destroy_spec.rb
@@ -3,10 +3,11 @@ require_relative '../../../../apps/web/controllers/article/destroy'
 
 describe Web::Controllers::Article::Destroy do
   let(:action) { Web::Controllers::Article::Destroy.new }
-  let(:params) { Hash[] }
+  let(:article) { create(:article) }
+  let(:params) { {id: article.id} }
 
   it 'is successful' do
     response = action.call(params)
-    response[0].must_equal 200
+    response[0].must_equal 302
   end
 end
