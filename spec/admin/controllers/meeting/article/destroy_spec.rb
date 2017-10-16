6673f2d5b0cf1685c6abe226bfbc2dcfdfab3f61
bo40 <bo40.git@gmail.com>
2017-10-16 09:04:11 +0900

    adminのarticle#destroyアクションのテストを実装した

--- spec/admin/controllers/meeting/article/destroy_spec.rb
+++ spec/admin/controllers/meeting/article/destroy_spec.rb
@@ -3,10 +3,14 @@ require_relative '../../../../../apps/admin/controllers/meeting/article/destroy'
 
 describe Admin::Controllers::Meeting::Article::Destroy do
   let(:action) { Admin::Controllers::Meeting::Article::Destroy.new }
-  let(:params) { Hash[] }
+  let(:article_repo) { ArticleRepository.new }
+  let(:article) { create(:article) }
+  let(:params) { {meeting_id: article.meeting_id, id: article.id} }
 
   it 'is successful' do
     response = action.call(params)
-    response[0].must_equal 200
+    response[0].must_equal 302
+
+    article_repo.find(article.id).must_equal nil
   end
 end
