f05d253e33e157bbcf16f8b090140cf5fbac7c14
bo40 <bo40.git@gmail.com>
2017-10-22 21:05:07 +0900

    コメントを修正した

--- lib/kumanodocs_hanami/repositories/article_repository.rb
+++ lib/kumanodocs_hanami/repositories/article_repository.rb
@@ -10,7 +10,7 @@ class ArticleRepository < Hanami::Repository
   end
 
   def update_number(meeting_id, articles_number)
-    # numberをnilで初期化してからupdateする
+    # データペースのUNIQUE制約にひっかからないようにすべてnilで初期化する
     articles.where(meeting_id: meeting_id).update(number: nil)
     articles_number.each do |article_attr|
       num = article_attr['number'].eql?("") ? nil : article_attr['number']
