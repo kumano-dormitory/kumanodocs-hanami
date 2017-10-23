ccbc1ef8bbeab2d171e7838a880339c3677c19a9
bo40 <bo40.git@gmail.com>
2017-10-24 04:00:06 +0900

    MeetingEntityのsorted_articlesのテストを書いた

--- spec/kumanodocs_hanami/entities/meeting_spec.rb
+++ spec/kumanodocs_hanami/entities/meeting_spec.rb
@@ -2,4 +2,20 @@ require 'spec_helper'
 
 describe Meeting do
   # place your tests here
+  it 'sorted_articlesでソートが正しく行われていること' do
+    numbers = [1, 2, 3].shuffle
+    articles = [
+      create(:article, number: numbers[0]),
+      create(:article, number: numbers[1]),
+      create(:article, number: numbers[2]),
+      create(:article, number: nil)
+    ]
+    meeting = Meeting.new(articles: articles)
+    sorted_articles = meeting.sorted_articles
+    sorted_articles.each_with_index do |article, index|
+      if article.number != nil
+        article.number.must_equal index + 1
+      end
+    end
+  end
 end
