95713f39acb970e8e15d8792d55c631e10261e6f
bo40 <bo40.git@gmail.com>
2017-10-24 04:26:56 +0900

    MeetingRepositoryのupdate_numberメソッドのテストを書いた

--- spec/kumanodocs_hanami/repositories/article_repository_spec.rb
+++ spec/kumanodocs_hanami/repositories/article_repository_spec.rb
@@ -9,11 +9,12 @@ describe ArticleRepository do
 
   let(:article) { create(:article) }
   let(:categories) { create_list(:category, 5) }
+  let(:meeting) { create(:meeting) }
 
   it 'categoryを追加できること' do
     datas = categories.sample(2).map { |category| { category_id: category.id } }
     article_repo.add_categories(article, datas)
-    
+
     _article = article_repo.find_with_relations(article.id)
     _article.article_categories.size.must_equal 2
   end
@@ -30,5 +31,20 @@ describe ArticleRepository do
     aggr_article = article_repo.find_with_relations(article.id)
     aggr_article.article_categories.wont_be_nil
     aggr_article.author.wont_be_nil
+  end
+
+  it 'article_numberの変更ができること' do
+    meeting_id = meeting.id
+    create_list(:article, 5, meeting_id: meeting_id)
+    meeting_with_articles = meeting_repo.find_with_articles(meeting_id)
+    max = meeting_with_articles.articles.count
+    numbers = [*1..max].shuffle
+    articles_number = meeting_with_articles.articles.map.with_index do |article, index|
+      {'article_id' => article.id, 'number' => numbers[index]}
+    end
+    article_repo.update_number(meeting_id, articles_number)
+    meeting_repo.find_with_articles(meeting_id).articles.each_with_index do |article, index|
+      article.number.must_equal numbers[index]
+    end
   end
 end
