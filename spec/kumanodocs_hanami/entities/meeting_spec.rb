3ee6a623d1c13c675571c608d1a2aff16b60a2de
bo40 <bo40.git@gmail.com>
2017-10-26 01:03:25 +0900

    articles_for_webのテストでメソッドを定義して４つのパターンをテスト

--- spec/kumanodocs_hanami/entities/meeting_spec.rb
+++ spec/kumanodocs_hanami/entities/meeting_spec.rb
@@ -3,16 +3,19 @@ require 'spec_helper'
 describe Meeting do
   # place your tests here
   it 'articles_for_webでソートが正しく行われていること' do
-    numbers = [1, 2, 3, nil].shuffle
-    articles = [
-      Article.new(number: numbers[0]),
-      Article.new(number: numbers[1]),
-      Article.new(number: numbers[2]),
-      Article.new(number: numbers[3])
-    ]
-    meeting = Meeting.new(articles: articles)
-    articles_for_web = meeting.articles_for_web
-    expected_numbers = [1, 2, 3, nil]
-    articles_for_web.map(&:number).must_equal expected_numbers
+    def test_articles_for_web(test_numbers)
+      shuffled_test_numbers = test_numbers.shuffle
+      articles = test_numbers.map { |number|
+        Article.new(number: number)
+      }
+      meeting = Meeting.new(articles: articles)
+      articles_for_web = meeting.articles_for_web
+      articles_for_web.map(&:number).must_equal test_numbers
+    end
+
+    test_articles_for_web([1, 2, 3, 4, 5])
+    test_articles_for_web([2, 3, 4, 6])
+    test_articles_for_web([1, 2, 3, nil])
+    test_articles_for_web([1, 2, 3, nil, nil])
   end
 end
