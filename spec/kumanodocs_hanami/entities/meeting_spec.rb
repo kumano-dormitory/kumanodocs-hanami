0ba0732b2d1fc8d4d5d1f93a5b9154174591fb0a
bo40 <bo40.git@gmail.com>
2017-11-02 06:01:35 +0900

    Meeting_specのテスト用メソッドの定義場所を修正

--- spec/kumanodocs_hanami/entities/meeting_spec.rb
+++ spec/kumanodocs_hanami/entities/meeting_spec.rb
@@ -1,21 +1,20 @@
 require 'spec_helper'
 
 describe Meeting do
-  # place your tests here
-  it 'articles_for_webでソートが正しく行われていること' do
-    def test_articles_for_web(test_numbers)
-      shuffled_test_numbers = test_numbers.shuffle
-      articles = test_numbers.map { |number|
-        Article.new(number: number)
-      }
-      meeting = Meeting.new(articles: articles)
-      articles_for_web = meeting.articles_for_web
-      articles_for_web.map(&:number).must_equal test_numbers
-    end
+  def assert_articles_for_web(test_numbers)
+    shuffled_test_numbers = test_numbers.shuffle
+    articles = test_numbers.map { |number|
+      Article.new(number: number)
+    }
+    meeting = Meeting.new(articles: articles)
+    articles_for_web = meeting.articles_for_web
+    articles_for_web.map(&:number).must_equal test_numbers
+  end
 
-    test_articles_for_web([1, 2, 3, 4, 5])
-    test_articles_for_web([2, 3, 4, 6])
-    test_articles_for_web([1, 2, 3, nil])
-    test_articles_for_web([1, 2, 3, nil, nil])
+  it 'articles_for_webでソートが正しく行われていること' do
+    assert_articles_for_web([1, 2, 3, 4, 5])
+    assert_articles_for_web([2, 3, 4, 6])
+    assert_articles_for_web([1, 2, 3, nil])
+    assert_articles_for_web([1, 2, 3, nil, nil])
   end
 end
