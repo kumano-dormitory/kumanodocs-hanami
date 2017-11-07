84e541d035dfd323882e72ecc0962cb5ca3ccc85
bo40 <bo40.git@gmail.com>
2017-11-07 16:19:34 +0900

    meetingとそれに付随するarticleの作成をassert_update_articles_numberメソッドへ移動した

--- spec/kumanodocs_hanami/repositories/article_repository_spec.rb
+++ spec/kumanodocs_hanami/repositories/article_repository_spec.rb
@@ -1,17 +1,21 @@
 require 'spec_helper'
 
 describe ArticleRepository do
-  def assert_update_articles_number(meeting_id, test_numbers)
-    meeting = meeting_repo.find_with_articles(meeting_id)
+  def assert_update_articles_number(before_numbers, test_numbers)
+    meeting = create(:meeting)
+    before_numbers.shuffle.each do |number|
+      create(:article, meeting_id: meeting.id, number: number)
+    end
+    meeting = meeting_repo.find_with_articles(meeting.id)
     shuffled_numbers = test_numbers.shuffle
 
     articles_number = meeting.articles.zip(shuffled_numbers).map do |article, number|
       {'article_id' => article.id, 'number' => number}
     end
-    article_repo.update_number(meeting_id, articles_number)
+    article_repo.update_number(meeting.id, articles_number)
 
     # 議案の番号が正しく変更されていることを確認
-    meeting_repo.find_with_articles(meeting_id).articles.zip(shuffled_numbers).map do |article, number|
+    meeting_repo.find_with_articles(meeting.id).articles.zip(shuffled_numbers).map do |article, number|
       article.number.must_equal number
     end
   end
@@ -47,22 +51,12 @@ describe ArticleRepository do
 
   it 'article_numberの変更ができること' do
     # 順序番号が変更前には全てnilの場合
-    meeting = create(:meeting)
-    create_list(:article, 5, meeting_id: meeting.id, number: nil)
-    assert_update_articles_number(meeting.id, [1, 2, 3, 4, 5])
+    assert_update_articles_number([nil, nil, nil, nil, nil], [1, 2, 3, 4, 5])
 
     # 順序番号が変更前には一部がnilである場合
-    meeting = create(:meeting)
-    [1, 2, 3, nil, nil].shuffle.each do |number|
-      create(:article, meeting_id: meeting.id, number: number)
-    end
-    assert_update_articles_number(meeting.id, [1, 2, 3, 4, 5])
+    assert_update_articles_number([1, 2, 3, nil, nil], [1, 2, 3, 4, 5])
 
     # 順序番号が変更前には全てnilではない場合
-    meeting = create(:meeting)
-    [1, 2, 3, 4, 5].shuffle.each do |number|
-      create(:article, meeting_id: meeting.id, number: number)
-    end
-    assert_update_articles_number(meeting.id, [1, 2, 3, 4, 5])
+    assert_update_articles_number([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
   end
 end
