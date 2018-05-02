b7677bc291d364e4b194f0c615c5026e3693058e
bo40 <bo40.git@gmail.com>
2018-05-02 21:59:10 +0900

    web#article#lock#newのコントローラのテストを実装

--- spec/web/controllers/article/lock/new_spec.rb
+++ spec/web/controllers/article/lock/new_spec.rb
@@ -2,12 +2,18 @@ require 'spec_helper'
 require_relative '../../../../../apps/web/controllers/article/lock/new'
 
 describe Web::Controllers::Article::Lock::New do
-  let(:action) { Web::Controllers::Article::Lock::New.new }
-  let(:params) { Hash[] }
+  let(:author) { Author.new(id: rand(1..5), lock: nil) }
+  let(:article) { Article.new(id: rand(1..5), author_id: author.id, author: author) }
 
   it 'is successful' do
-    skip
-    response = action.call(params)
+    article_repo = MiniTest::Mock.new.expect(:find_with_relations, article, [article.id])
+    action = Web::Controllers::Article::Lock::New.new(
+      article_repo: article_repo
+    )
+    response = action.call({article_id: article.id})
+
     response[0].must_equal 200
+    article_repo.verify.must_equal true
+    action.locked.must_equal false
   end
 end
