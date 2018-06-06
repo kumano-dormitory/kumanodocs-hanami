a1733dd7176a35e4adafb86c8423265596225060
bo40 <bo40.git@gmail.com>
2018-06-07 08:32:49 +0900

    hanami generate action web comment#editを実行した

---
+++ apps/web/controllers/comment/edit.rb
@@ -0,0 +1,8 @@
+module Web::Controllers::Comment
+  class Edit
+    include Web::Action
+
+    def call(params)
+    end
+  end
+end
---
+++ apps/web/views/comment/edit.rb
@@ -0,0 +1,5 @@
+module Web::Views::Comment
+  class Edit
+    include Web::View
+  end
+end
