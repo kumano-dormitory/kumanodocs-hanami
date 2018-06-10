b9c42597d5c2d1f9144205b87c5d5b224e3fa822
bo40 <bo40.git@gmail.com>
2018-06-10 23:04:29 +0900

    議案idとブロックidから議事録を検索するメソッドをレポジトリに追加

--- lib/kumanodocs_hanami/repositories/comment_repository.rb
+++ lib/kumanodocs_hanami/repositories/comment_repository.rb
@@ -1,2 +1,10 @@
 class CommentRepository < Hanami::Repository
+  def find(article_id, block_id)
+    comments
+      .where(article_id: article_id, block_id: block_id)
+      .order(:updated_at)
+      .reverse
+      .to_a
+      .first
+  end
 end
