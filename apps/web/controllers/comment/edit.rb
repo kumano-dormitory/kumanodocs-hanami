8c3968fc2ce6b9da8f404fb165704908c607f366
bo40 <bo40.git@gmail.com>
2018-06-10 23:06:10 +0900

    comment#editを実装した

--- apps/web/controllers/comment/edit.rb
+++ apps/web/controllers/comment/edit.rb
@@ -1,8 +1,21 @@
 module Web::Controllers::Comment
   class Edit
     include Web::Action
+    expose :meeting, :block_id, :comment_datas
+
+    def initialize(meeting_repo: MeetingRepository.new,
+                   comment_repo: CommentRepository.new)
+      @meeting_repo = meeting_repo
+      @comment_repo = comment_repo
+    end
 
     def call(params)
+      @meeting = @meeting_repo.find_with_articles(params[:meeting_id])
+      @block_id = params[:block_id]
+      @comment_datas = @meeting.articles.map do |article|
+        comment = @comment_repo.find(article.id, params[:block_id])
+        { article_id: article.id, comment: comment&.body }
+      end
     end
   end
 end
--- apps/web/views/comment/edit.rb
+++ apps/web/views/comment/edit.rb
@@ -1,5 +1,8 @@
+require_relative './form'
+
 module Web::Views::Comment
   class Edit
     include Web::View
+    include Form
   end
 end
