59a7a18bb18154154b17f47cea09682e271b4b6e
bo40 <bo40.git@gmail.com>
2019-04-11 20:43:21 +0900

    adminにpdfダウンロードページのプロトタイプを作成した
    現状ではlatexコードを表示するのみ

--- apps/admin/controllers/meeting/download.rb
+++ apps/admin/controllers/meeting/download.rb
@@ -1,14 +1,19 @@
 module Admin::Controllers::Meeting
   class Download
     include Admin::Action
-    expose :meeting
+    expose :meeting, :tex
 
     def initialize(meeting_repo: MeetingRepository.new)
       @meeting_repo = meeting_repo
     end
 
     def call(params)
-      @meeting = @meeting_repo.find_with_articles(params[:id])
+      @meeting = @meeting_repo.find_with_printed_articles(params[:id])
+      @tex = @meeting.to_tex
+      # write_file(temp, @tex)
+      # run_command(ptex2pdf -u -l, temp)
+      # self.format = :pdf
+      # unsafe_send_file Pathname.new("/path/to/pdf")
       self.format = :txt
     end
   end
--- lib/kumanodocs_hanami/entities/meeting.rb
+++ lib/kumanodocs_hanami/entities/meeting.rb
@@ -4,4 +4,37 @@ class Meeting < Hanami::Entity
     articles_with_integer_number.sort_by! { |article| article.number }
     articles_with_integer_number.concat(articles_with_nil_number)
   end
+
+  def to_tex
+    head = <<-EOS
+\\documentclass[b5j,10pt]{ujarticle}
+\\usepackage{listings}
+\\usepackage{spverbatim}
+\\usepackage{tocloft}
+\\usepackage{here}
+\\usepackage[top=5truemm,bottom=15truemm,left=5truemm,right=5truemm]{geometry}
+
+\\lstset{
+breaklines=true
+1;2802;0c}
+\\renewcommand{\\cftsecleader}{\\cftdotfill{\\cftdotsep}}
+EOS
+
+    head = head + "\\title{ #{date} ブロック会議 }" + <<-EOS
+\\date{}
+\\author{}
+
+\\begin{document}
+\\maketitle
+\\vspace{-10ex}
+\\tableofcontents
+EOS
+
+    body = articles.map { |article|
+      "\\section*{ #{article.title} \\\\ \\ \\  \\normalsize{ 文責：#{article.author_id} } }\n\\addcontentsline{toc}{section}{\\protect\\numberline{} #{article.title} }%\n\\begin{spverbatim}#{article.body}"
+    }.join("\n")
+
+    tail = "\n\\end{document}"
+    head + body + tail
+  end
 end
