2d94bf13ea0967c643c8f6552bac0becb122150e
bo40 <bo40.git@gmail.com>
2019-04-13 01:47:37 +0900

    adminのルーティングにtableモデルの操作ページを追加

--- apps/admin/config/routes.rb
+++ apps/admin/config/routes.rb
@@ -5,8 +5,11 @@
 # get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
 root to: 'meeting#index'
 resources :meeting, only: %i[index show new create destroy] do
-  resources :article
+  resources :article do
+    resources :table, only: [:new, :create, :edit, :update, :destroy]
+  end
 end
+
 get '/meeting/:id/articles/number/edit', to: 'article_number#edit', as: :edit_article_number
 patch '/meeting/:id/articles/number/', to: 'article_number#update', as: :article_number
 get '/meeting/:id/articles/status/edit', to: 'article_status#edit', as: :edit_article_status
