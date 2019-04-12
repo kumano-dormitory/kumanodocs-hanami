1e8d7a8d136ec8011b2c1481b6126c285be3d704
bo40 <bo40.git@gmail.com>
2019-04-12 20:19:52 +0900

    table#editアクションの実装

---
+++ apps/web/controllers/table/edit.rb
@@ -0,0 +1,21 @@
+module Web::Controllers::Table
+  class Edit
+    include Web::Action
+    expose :table
+
+    def initialize(table_repo: TableRepository.new)
+      @table_repo = table_repo
+    end
+
+    def call(params)
+      @table = @table_repo.find_with_relations(params[:id])
+      if table.article.author.locked? && table.article.author.lock_key == cookies[:article_lock_key]
+      else
+        redirect_to routes.new_article_lock_for_table_path(
+          article_id: table.article.id,
+          table_id: table.id
+        )
+      end
+    end
+  end
+end
