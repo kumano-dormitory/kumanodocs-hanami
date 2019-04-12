8fda366a6ea83c2792c58d80e0e68a5ce0b48629
bo40 <bo40.git@gmail.com>
2019-04-12 20:18:50 +0900

    table#createの実装

---
+++ apps/web/controllers/table/create.rb
@@ -0,0 +1,47 @@
+require 'csv'
+
+module Web::Controllers::Table
+  class Create
+    include Web::Action
+    expose :articles
+
+    params do
+      required(:table).schema do
+        required(:article_id).filled(:int?)
+        required(:article_passwd).filled(:str?)
+        required(:caption).filled(:str?)
+        required(:tsv).filled(:str?)
+      end
+    end
+
+    def initialize(article_repo: ArticleRepository.new,
+                   table_repo: TableRepository.new)
+      @article_repo = article_repo
+      @table_repo = table_repo
+    end
+
+    def call(params)
+      if params.valid?
+        article = @article_repo.find_with_relations(params[:table][:article_id])
+        if (article.meeting.deadline > Time.now)
+          begin
+            CSV.parse(params[:table][:tsv], col_sep: "\t")
+            if article.author.authenticate(params[:table][:article_passwd])
+              @table_repo.create(
+                article_id: params[:table][:article_id],
+                caption: params[:table][:caption],
+                csv: params[:table][:tsv],
+              )
+              redirect_to routes.article_path(id: params[:table][:article_id])
+            end
+          rescue CSV::MalformedCSVError
+            @articles = @article_repo.before_deadline
+            self.status = 422
+          end
+        end
+      end
+      @articles = @article_repo.before_deadline
+      self.status = 422
+    end
+  end
+end
--- lib/kumanodocs_hanami/repositories/table_repository.rb
+++ lib/kumanodocs_hanami/repositories/table_repository.rb
@@ -1,2 +1,18 @@
 class TableRepository < Hanami::Repository
+  associations do
+    belongs_to :article
+  end
+
+  def find_with_relations(id)
+    table = aggregate(:article)
+            .tables
+            .where(id: id)
+            .map_to(Table).one
+    author = AuthorRepository.new.find(table.article.author_id)
+    Table.new(
+      table.to_h.merge(article: {author: author}) { |_, oldval, newval|
+        oldval.to_h.merge(newval)
+      }
+    )
+  end
 end
