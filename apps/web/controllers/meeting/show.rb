18fcdf059a969be246b13f0e64d4d3d4dd101bd0
bo40 <bo40.git@gmail.com>
2019-04-17 14:04:23 +0900

    web meeting#show action implimentation of pagenation

--- apps/web/controllers/meeting/show.rb
+++ apps/web/controllers/meeting/show.rb
@@ -1,14 +1,17 @@
 module Web::Controllers::Meeting
   class Show
     include Web::Action
-    expose :meeting
+    expose :meeting, :page, :max_page, :article_limit
 
-    def initialize(meeting_repo: MeetingRepository.new)
+    def initialize(meeting_repo: MeetingRepository.new, article_limit: 10)
       @meeting_repo = meeting_repo
+      @article_limit = article_limit
     end
 
     def call(params)
+      @page = params[:page]&.to_i || 1
       @meeting = @meeting_repo.find_with_articles(params[:id])
+      @max_page = (meeting.articles.length - 1) / @article_limit + 1
     end
   end
 end
