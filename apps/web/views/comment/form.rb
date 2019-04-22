973d89c90d1b1694f5607fc4f6b04ba6b294c345
bo40 <bo40.git@gmail.com>
2019-04-22 20:23:01 +0900

    commentのフォームをvanilla frameworkに対応させた

--- apps/web/views/comment/form.rb
+++ apps/web/views/comment/form.rb
@@ -4,10 +4,26 @@ module Web::Views::Comment
 
       form_for :comments,
                routes.comments_path(meeting_id: meeting.id, block_id: block_id),
-               method: :patch do
-        div do
-          label "パスワード"
-          password_field :password
+               method: :patch,
+               class: "p-form" do
+
+        if !params.valid? && params.errors.dig(:comments, :password)
+          div class: "p-form-validation is-error" do
+            label "パスワード", for: :password
+            password_field :password, class: "p-form-validation__input"
+            p class: "p-form-validation__message" do
+              if params.errors.dig(:comments, :password).include?("must be filled")
+                strong "この項目は必須です"
+              else
+                "入力が不正です 文字列を入力してください"
+              end
+            end
+          end
+        else
+          div do
+            label "パスワード", for: :password
+            password_field :password
+          end
         end
 
         meeting.articles.each_with_index do |article, idx|
@@ -21,7 +37,8 @@ module Web::Views::Comment
               text_area :comment,
                         "#{comment_data.nil? ? '' : comment_data[:comment]}",
                         name: "meeting[articles][][comment]",
-                        id: "meeting-articles-#{idx}-comment"
+                        id: "meeting-articles-#{idx}-comment",
+                        rows: 10
               hidden_field :article_id, value: article.id,
                         name: "meeting[articles][][article_id]",
                         id: "meeting-articles-#{idx}-article_id"
