527d12fb36583d2fa7722f342f4c573ef093a61f
bo40 <bo40.git@gmail.com>
2019-04-22 20:07:50 +0900

    flashを有効にしてnavigationをページの最初に表示できるように実装

--- apps/web/controllers/base.rb
+++ apps/web/controllers/base.rb
@@ -2,12 +2,25 @@ module Web
   module BaseController
     def self.included(action)
       action.class_eval do
-        expose :navigation
+        expose :navigation, :notifications
       end
     end
 
     def navigation
       @navigation = {}
+    end
+
+    def notifications
+      if flash[:notifications]
+        @notifications = flash[:notifications].map { |key,val|
+          new_val = val.map{ |k,v| [k.to_sym, v] }.to_h
+          [key.to_sym, new_val]
+        }.to_h
+        flash.clear
+      else
+        @notifications = {}
+      end
+      @notifications
     end
   end
 end
