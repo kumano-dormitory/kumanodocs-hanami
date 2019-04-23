9eebcc606fb8b12855b19d4e3d02d612908520e1
bo40 <bo40.git@gmail.com>
2019-04-23 15:12:28 +0900

    admin meeting#createにnotificationsの実装

--- apps/admin/controllers/meeting/create.rb
+++ apps/admin/controllers/meeting/create.rb
@@ -11,16 +11,23 @@ module Admin::Controllers::Meeting
 
     def initialize(meeting_repo: MeetingRepository.new)
       @meeting_repo = meeting_repo
+      @notifications = {}
     end
 
     def call(params)
       if params.valid?
         @meeting_repo.create(params[:meeting])
 
+        flash[:notifications] = {success: {status: "Success:", message: "正常にブロック会議日程が作成されました"}}
         redirect_to routes.meetings_path
       else
+        @notifications = {error: {status: "Error:", message: "入力された項目に不備があります. もう一度確認してください"}}
         self.status = 422
       end
+    end
+
+    def notifications
+      @notifications
     end
   end
 end
