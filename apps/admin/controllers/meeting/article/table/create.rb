194aba20ea04ed6e34aa83b044db9243fce7444e
bo40 <bo40.git@gmail.com>
2019-04-24 23:33:40 +0900

    admin meeting#article#tableのアクションをvanilla frameworkに対応させた

--- apps/admin/controllers/meeting/article/table/create.rb
+++ apps/admin/controllers/meeting/article/table/create.rb
@@ -19,6 +19,7 @@ module Admin::Controllers::Meeting
                        article_repo: ArticleRepository.new)
           @table_repo = table_repo
           @article_repo = article_repo
+          @notifications = {}
         end
 
         def call(params)
@@ -30,14 +31,22 @@ module Admin::Controllers::Meeting
                 caption: params[:table][:caption],
                 csv: params[:table][:tsv]
               )
+              flash[:notifications] = {success: {status: "Success:", message: "正常に表が追加されました"}}
               redirect_to routes.meeting_article_path(
                             meeting_id: params[:meeting_id],
                             id: params[:article_id])
             rescue CSV::MalformedCSVError
+              @notifications = {error: {status: "Error:", message: "表データの形式が不正です. 正しく入力してください"}}
             end
+          else
+            @notifications = {error: {status: "Error:", message: "入力された項目に不備があります. もう一度確認してください"}}
           end
           @article = @article_repo.find(params[:article_id])
           self.status = 422
+        end
+
+        def notifications
+          @notifications
         end
       end
     end
