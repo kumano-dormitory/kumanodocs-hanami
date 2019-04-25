0c7c0175934c43b01f7bb6fc1d797079f026c084
bo40 <bo40.git@gmail.com>
2019-04-26 00:05:27 +0900

    viewportの設定

---
+++ apps/admin/assets/javascripts/viewport.js
@@ -0,0 +1,28 @@
+var baseW = 980;	//基準となるブレークポイント
+var baseMin = 480;
+var iOSviewportW = 0;
+var ua = navigator.userAgent.toLowerCase();
+var isiOS = (ua.indexOf("iphone") > -1) || (ua.indexOf("ipod") > -1) || (ua.indexOf("ipad") > -1);
+if(isiOS){
+	iOSviewportW = document.documentElement.clientWidth;
+}
+function updateMetaViewport(){
+	var viewportContent;
+	var w = window.outerWidth;
+	if(isiOS){
+		w = iOSviewportW;
+	}
+  if (w > baseMin){
+    viewportContent = "width=device-width,initial-scale=1";
+  }else{
+    viewportContent = "width="+baseMin+"px,initial-scale="+(w/baseMin);
+  }
+	document.querySelector("meta[name='viewport']").setAttribute("content", viewportContent);
+}
+//イベントハンドラ登録
+window.addEventListener("resize", updateMetaViewport, false);
+window.addEventListener("orientationchange", updateMetaViewport, false);
+//初回イベント強制発動
+var ev = document.createEvent("UIEvent");
+ev.initEvent("resize", true, true)
+window.dispatchEvent(ev);
