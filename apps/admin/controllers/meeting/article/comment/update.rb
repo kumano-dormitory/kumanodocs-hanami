7c85005712447ee096a07d0eef74a40098a83b5a
bo40 <bo40.git@gmail.com>
2019-05-03 21:16:32 +0900

    adminで採決結果の表示ができるようにした

--- apps/admin/controllers/meeting/article/comment/update.rb
+++ apps/admin/controllers/meeting/article/comment/update.rb
@@ -3,26 +3,32 @@ module Admin::Controllers::Meeting
     module Comment
       class Update
         include Admin::Action
-        expose :article, :block, :comment
+        expose :article, :block, :comment, :vote_result
 
         params do
           required(:article_id).filled(:int?)
           required(:block_id).filled(:int?)
           required(:comment).schema do
             required(:body).filled(:str?)
+            optional(:agree) { filled? & int? & gteq?(0) }
+            optional(:disagree) { filled? & int? & gteq?(0) }
+            optional(:onhold) { filled? & int? & gteq?(0) }
           end
         end
 
         def initialize(article_repo: ArticleRepository.new,
                        block_repo: BlockRepository.new,
-                       comment_repo: CommentRepository.new)
+                       comment_repo: CommentRepository.new,
+                       vote_result_repo: VoteResultRepository.new)
           @article_repo = article_repo
           @block_repo = block_repo
           @comment_repo = comment_repo
+          @vote_result_repo = vote_result_repo
           @notifications = {}
         end
 
         def call(params)
+          @article = @article_repo.find_with_relations(params[:article_id])
           if params.valid?
             props = {
               article_id: params[:article_id],
@@ -37,12 +43,31 @@ module Admin::Controllers::Meeting
               # 議事録のupdate
               @comment_repo.update(comment.id, props)
             end
+
+            # 採決結果の保存
+            props = {
+              article_id: params[:article_id],
+              block_id: params[:block_id],
+              agree: params[:comment][:agree],
+              disagree: params[:comment][:disagree],
+              onhold: params[:comment][:onhold]
+            }
+            if props[:agree] && props[:disagree] && props[:onhold] && \
+              @article.categories.find{ |category| category.require_content && category.name == '採決' }
+
+              vote_result = @vote_result_repo.find(params[:article_id], params[:block_id])
+              if vote_result.nil?
+                @vote_result_repo.create(props.merge({crypt_password: '1'}))
+              else
+                @vote_result_repo.update(vote_result.id, props)
+              end
+            end
             flash[:notifications] = {success: {status: "Success:", message: "正常に議事録が編集されました"}}
             redirect_to routes.meeting_article_path(meeting_id: 0, id: params[:article_id])
           else
-            @article = @article_repo.find(params[:article_id])
             @block = @block_repo.find(params[:block_id])
             @comment = @comment_repo.find(params[:article_id], params[:block_id])
+            @vote_result = VoteResult.new(params[:comment])
             @notifications = {error: {status: "Error:", message: "入力された項目に不備があります. もう一度確認してください"}}
             self.status = 422
           end
--- apps/admin/views/meeting/article/comment/form.rb
+++ apps/admin/views/meeting/article/comment/form.rb
@@ -2,7 +2,7 @@ module Admin::Views::Meeting
   module Article
     module Comment
       module Form
-        def form_update(article_id, block_id, comment)
+        def form_update(article_id, block_id, comment, vote_result = nil)
 
           form_for :comment,
                    routes.comment_path(article_id: article_id, block_id: block_id),
@@ -10,6 +10,22 @@ module Admin::Views::Meeting
             div do
               text_area :body, "#{comment.nil? ? '' : comment.body}", rows: 15
             end
+
+            if vote_result
+              div do
+                p '採決結果'
+                label '賛成', for: :agree
+                number_field :agree, value: vote_result.agree,
+                             min: 0, step: 1, required: ""
+                label '反対', for: :disagree
+                number_field :disagree, value: vote_result.disagree,
+                            min: 0, step: 1, required: ""
+                label '保留', for: :onhold
+                number_field :onhold, value: vote_result.onhold,
+                             min: 0, step: 1, required: ""
+              end
+            end
+
             submit '確定', class: "p-button--positive"
           end
         end
