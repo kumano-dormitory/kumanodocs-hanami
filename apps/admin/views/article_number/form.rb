c2d001ae2a1f340ad6c185fd97752048d3623ebb
bo40 <bo40.git@gmail.com>
2019-05-04 22:40:04 +0900

    admin印刷用PDFダウンロードページの実装
    ダウンロードページをブロック会議選択→資料か議事録か選択→並び替え→ダウンロード
    という遷移でダウンロードできるように実装した

--- apps/admin/views/article_number/form.rb
+++ apps/admin/views/article_number/form.rb
@@ -1,16 +1,17 @@
 module Admin::Views::ArticleNumber
   module Form
-    def form(meeting)
+    def form(meeting, for_download)
       return if meeting.articles.nil?
       max = meeting.articles.count
+      button_text = for_download ? '議案順序を確定して資料をダウンロード' : '議案順序を保存'
 
       form_for :meeting,
-               routes.article_number_path(id: meeting.id),
+               routes.article_number_path(id: meeting.id) + (for_download ? "?download=true" : ''),
                id: "form_for_article_order",
                method: :patch, class: "p-form" do
         hidden_field :meeting_id, value: meeting.id
 
-        button '議案順序を保存', id: "post_article_order_btn", class: "p-button--positive"
+        button button_text, id: "post_article_order_btn", class: "p-button--positive"
       end
     end
   end
