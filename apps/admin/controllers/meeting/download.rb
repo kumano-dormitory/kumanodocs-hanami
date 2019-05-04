7be424e97af652fd13ba8bc0384a53e218f487f2
bo40 <bo40.git@gmail.com>
2019-05-04 22:40:04 +0900

    admin印刷用PDFダウンロードページの実装
    ダウンロードページをブロック会議選択→資料か議事録か選択→並び替え→ダウンロード
    という遷移でダウンロードできるように実装した

--- apps/admin/controllers/meeting/download.rb
+++ apps/admin/controllers/meeting/download.rb
@@ -1,23 +1,54 @@
 module Admin::Controllers::Meeting
   class Download
     include Admin::Action
-    expose :tex_str
+    expose :meetings, :meeting, :tex_str, :view_type
+
+    params do
+      required(:id) { filled? & int? & gt?(0) }
+      optional(:articles).filled(:bool?)
+      optional(:comments).filled(:bool?)
+    end
 
     def initialize(meeting_repo: MeetingRepository.new,
-                   article_repo: ArticleRepository.new)
+                   article_repo: ArticleRepository.new,
+                   block_repo: BlockRepository.new)
       @meeting_repo = meeting_repo
       @article_repo = article_repo
+      @block_repo = block_repo
     end
 
     def call(params)
-      @meeting = @meeting_repo.find_with_articles(params[:id])
-      @articles = @meeting.articles.map{ |article| @article_repo.find_with_relations(article.id) }
-      @tex_str = Admin::Views::Meeting::Download.render(format: :tex, meeting: @meeting, articles: @articles)
-      # write_file(temp, @tex)
-      # run_command(ptex2pdf -u -l, temp)
-      # self.format = :pdf
-      # unsafe_send_file Pathname.new("/path/to/pdf")
-      self.format = :txt
+      if params.valid?
+        @meeting = @meeting_repo.find_with_articles(params[:id])
+        if params[:articles]
+          @articles = @meeting.articles.map{ |article| @article_repo.find_with_relations(article.id) }
+          @tex_str = Admin::Views::Meeting::Download.render(format: :tex, meeting: @meeting, articles: @articles, type: :articles)
+          # write_file(temp, @tex)
+          # run_command(ptex2pdf -u -l, temp)
+          # self.format = :pdf
+          # unsafe_send_file Pathname.new("/path/to/pdf")
+          self.format = :txt
+        elsif params[:comments]
+          @articles = @meeting.articles.map{ |article| @article_repo.find_with_relations(article.id) }
+          @blocks = @block_repo.all
+          @tex_str = Admin::Views::Meeting::Download.render(format: :tex, meeting: @meeting, articles: @articles, blocks: @blocks, type: :comments)
+          # write_file(temp, @tex)
+          # run_command(ptex2pdf -u -l, temp)
+          # self.format = :pdf
+          # unsafe_send_file Pathname.new("/path/to/pdf")
+          self.format = :txt
+        else
+          @view_type = :meeting
+        end
+      else
+        p params.errors.to_s
+        @meetings = @meeting_repo.desc_by_date(limit: 20)
+        @view_type = :meetings
+      end
+    end
+
+    def navigation
+      @navigation = {pdf: true}
     end
   end
 end
