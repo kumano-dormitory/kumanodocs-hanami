8818b469d2189262262b2ea53a9a8c07ee88c0d1
bo40 <bo40.git@gmail.com>
2019-05-04 17:39:13 +0900

    admin meeting#indexにページネーションを実装した

--- apps/admin/controllers/meeting/index.rb
+++ apps/admin/controllers/meeting/index.rb
@@ -1,14 +1,25 @@
 module Admin::Controllers::Meeting
   class Index
     include Admin::Action
-    expose :meetings
+    expose :meetings, :page
+
+    MEETING_COUNT_LIMIT = 15
+
+    params do
+      optional(:page) { filled? & int? & gt?(0) }
+    end
 
     def initialize(meeting_repo: MeetingRepository.new)
       @meeting_repo = meeting_repo
     end
 
     def call(params)
-      @meetings = @meeting_repo.desc_by_date
+      if params.valid?
+        @page = params[:page] || 1
+      else
+        @page = 1
+      end
+      @meetings = @meeting_repo.desc_by_date(limit: MEETING_COUNT_LIMIT, offset: MEETING_COUNT_LIMIT * (@page - 1))
     end
 
     def navigation
