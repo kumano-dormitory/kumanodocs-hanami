cc8a5e7502edfa55745d51aa42d8d48cd09b0521
bo40 <bo40.git@gmail.com>
2019-05-15 22:35:52 +0900

    議案投稿時にフォーマットがMarkdownならば、プレビューを表示するタブを表示するように修正

--- apps/web/assets/javascripts/markdown_preview.js
+++ apps/web/assets/javascripts/markdown_preview.js
@@ -1,27 +1,53 @@
-const previewContainer = document.getElementById('markdown-preview-container');
-const toggle = document.getElementById('article-format');
-
+const markdownTab = document.getElementById('markdown-tab');
 const preview = document.getElementById('markdown-preview');
+const toggle = document.getElementById('article-format');
 const toggle2 = document.getElementById('article-body');
+const tab1 = document.getElementById('tab-write');
+const tab2 = document.getElementById('tab-preview');
 
 function initializeMarkdownPreview() {
-  // 初期のプレビュー表示
+  // 初期のMarkdownプレビューのタブ表示
   if (toggle.checked) {
-    previewContainer.style.display="block";
+    markdownTab.style.display="block";
   } else {
-    previewContainer.style.display="none";
+    markdownTab.style.display="none";
   }
   preview.innerHTML = markdownParse(toggle2.value);
+  preview.style.display="none";
+  preview.style.minHeight="45rem";
 
+  // イベントハンドラの登録
   toggle.addEventListener('change', e => {
     if (e.target.checked) {
-      previewContainer.style.display="block";
+      markdownTab.style.display="block";
+      toggle2.style.display="block";
+      preview.style.display="none";
+      tab1.setAttribute('aria-selected', 'true');
+      tab2.setAttribute('aria-selected', 'false');
     } else {
-      previewContainer.style.display="none";
+      markdownTab.style.display="none";
+      toggle2.style.display="block";
+      preview.style.display="none";
+      tab1.setAttribute('aria-selected', 'true');
+      tab2.setAttribute('aria-selected', 'false');
     }
   });
   toggle2.addEventListener('input', e => {
     preview.innerHTML = markdownParse(e.target.value);
+  });
+  tab1.addEventListener('click', e => {
+    e.preventDefault();
+    toggle2.style.display="block";
+    preview.style.display="none";
+    tab1.setAttribute('aria-selected', 'true');
+    tab2.setAttribute('aria-selected', 'false');
+  });
+  tab2.addEventListener('click', e => {
+    e.preventDefault();
+    toggle2.style.display="none";
+    preview.style.display="block";
+    tab1.setAttribute('aria-selected', 'false');
+    tab2.setAttribute('aria-selected', 'true');
   });
 
 }
@@ -37,6 +63,9 @@ function olReplacer(match, p1) {
 }
 
 function markdownParse(markdownStr) {
+  if (markdownStr === "") {
+    return 'プレビューとして表示できるものがありません<br/>本文を入力してください';
+  }
   return markdownStr
      .replace(/&/, '&amp;')
      .replace(/</g, '&lt;')
@@ -53,6 +82,7 @@ function markdownParse(markdownStr) {
      .replace(/\n-{4,}/g, '<list-19273045-n><hr />')
      .replace(/<\/ul>\s?<ul>/g, '')
      .replace(/<\/ol>\s?<ol>/g, '')
+     .replace(/(<\/h4>|<\/h3>|<\/h2>)(\r\n|\r|\n)/g, '$1')
      .replace(/(\r\n|\r|\n)/g, '<br>\n')
      .replace(/(<\/ul>|<\/ol>)\s?<br>/g, '$1')
      .replace(/<list-19273045-n>/g, '\n');
