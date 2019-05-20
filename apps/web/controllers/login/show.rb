7716a6e1d31e198f3d0f7ff1d8a6a8e8b8152757
bo40 <bo40.git@gmail.com>
2019-05-20 16:11:58 +0900

    standalone起動させたときにtokenが維持されていればログインをするようにした

--- apps/web/controllers/login/show.rb
+++ apps/web/controllers/login/show.rb
@@ -5,6 +5,9 @@ module Web::Controllers::Login
 
     def call(params)
       @standalone = !!params[:standalone]
+      if @standalone && authenticated?
+        redirect_to routes.root_path
+      end
     end
 
     def authenticate!
