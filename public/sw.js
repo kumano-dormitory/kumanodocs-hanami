bad13e4761b7f17d2c7aabde19e4ae0b817128b6
bo40 <bo40.git@gmail.com>
2019-05-23 16:38:49 +0900

    service worker cache and update

--- public/sw.js
+++ public/sw.js
@@ -1,32 +1,49 @@
 var CACHE_NAME = 'kumanodocs-v1';
 var urlsToCache = [
   '/assets/application.css',
+  '/assets/bottom_navigation.js',
+  '/assets/contextual_menu.js',
   '/assets/viewport.js',
   '/assets/logo.png',
+  '/manifest.json',
 ];
 
 self.addEventListener('install', e => {
   // Perform install steps
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+  e.waitUntil(precache());
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          console.log('cache hit: ' + e.request.url);
-          return response;
-        }
-        return fetch(e.request);
-      }
-    )
-  );
+  e.respondWith(fromCache(e.request));
+  e.waitUntil(update(e.request));
 });
+
+function precache() {
+  return caches.open(CACHE_NAME).then(function(cache) {
+    console.log('Opened cache');
+    return cache.addAll(urlsToCache);
+  });
+}
+
+function fromCache(request) {
+  return caches.match(request).then(function (response) {
+    if (response) {
+      console.log('cache hit: ' + request.url);
+      return response;
+    }
+    return fetch(request);
+  });
+}
+
+function update(request) {
+  return caches.open(CACHE_NAME).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if (matching) {
+        return fetch(request).then(function (response) {
+          console.log('cache update: ' + request.url);
+          return cache.put(request, response);
+        });
+      }
+    });
+  });
+}
