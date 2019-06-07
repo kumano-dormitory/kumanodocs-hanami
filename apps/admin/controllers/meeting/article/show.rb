47425f685e130ae57ed9bd376c6e6545cbab9cc9
bo40 <bo40.git@gmail.com>
2019-06-07 20:11:35 +0900

    adminの議案詳細表示で議事録への返答も表示するように修正

--- apps/admin/controllers/meeting/article/show.rb
+++ apps/admin/controllers/meeting/article/show.rb
@@ -2,17 +2,20 @@ module Admin::Controllers::Meeting
   module Article
     class Show
       include Admin::Action
-      expose :article, :blocks
+      expose :article, :blocks, :messages
 
       def initialize(article_repo: ArticleRepository.new,
-                     block_repo: BlockRepository.new)
+                     block_repo: BlockRepository.new,
+                     message_repo: MessageRepository.new)
         @article_repo = article_repo
         @block_repo = block_repo
+        @message_repo = message_repo
       end
 
       def call(params)
         @article = @article_repo.find_with_relations(params[:id])
         @blocks = @block_repo.all
+        @messages = @message_repo.by_article(@article.id).group_by{|message| message.comment_id}
       end
     end
   end
