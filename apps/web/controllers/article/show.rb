df9c28e26c674bbb05a2f4629e70489eae9252f8
bo40 <bo40.git@gmail.com>
2019-06-09 14:37:27 +0900

    web article#showでの関連議案の表示実装

--- apps/web/controllers/article/show.rb
+++ apps/web/controllers/article/show.rb
@@ -1,22 +1,25 @@
 module Web::Controllers::Article
   class Show
     include Web::Action
-    expose :article, :blocks, :editable, :messages, :enable_html
+    expose :article, :blocks, :editable, :messages, :enable_html, :article_refs
 
     def initialize(article_repo: ArticleRepository.new,
                    block_repo: BlockRepository.new,
                    meeting_repo: MeetingRepository.new,
-                   message_repo: MessageRepository.new)
+                   message_repo: MessageRepository.new,
+                   article_reference_repo: ArticleReferenceRepository.new)
       @article_repo = article_repo
       @block_repo = block_repo
       @meeting_repo = meeting_repo
       @message_repo = message_repo
+      @article_reference_repo = article_reference_repo
     end
 
     def call(params)
       @article = @article_repo.find_with_relations(params[:id])
       @blocks = @block_repo.all
       @messages = @message_repo.by_article(@article.id).group_by{|message| message.comment_id}
+      @article_refs = @article_reference_repo.find_refs(@article.id)
       @editable = if after_deadline?
         # 追加議案の編集期間
         @meeting_repo.find_most_recent.id == @article.meeting_id && !@article.checked
