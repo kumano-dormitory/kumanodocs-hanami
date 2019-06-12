84d8c3daadeea34973fbbf9a262e0cf0d8068fb6
bo40 <bo40.git@gmail.com>
2019-06-12 14:53:28 +0900

    content-security-polityを修正し、必要なアクションのみに権限を与えた

--- apps/web/controllers/article/diff.rb
+++ apps/web/controllers/article/diff.rb
@@ -19,6 +19,9 @@ module Web::Controllers::Article
       if params.valid?
         @article_old = @article_repo.find_with_relations(params[:diff][:old_article])
         @article_new = @article_repo.find_with_relations(params[:diff][:new_article])
+        self.headers.merge!({
+          'Content-Security-Policy' => "form-action 'self'; frame-ancestors 'self'; base-uri 'self'; default-src 'none'; manifest-src 'self'; script-src 'self' https://cdnjs.cloudflare.com/ajax/libs/jsdiff/ https://cdnjs.cloudflare.com/ajax/libs/diff2html/; connect-src 'self'; img-src 'self' https: data:; style-src 'self' 'unsafe-inline' https:; font-src 'self'; object-src 'none'; plugin-types application/pdf; child-src 'self'; frame-src 'self'; media-src 'self'"
+        })
       else
         @notifications = {caution: {status: '注意:', message: '議案を2つ選択してください'}}
         @recent_meetings_with_articles = @article_repo.group_by_meeting(16, months: 6)
