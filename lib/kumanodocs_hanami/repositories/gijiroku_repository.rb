69c1e91d4605a3f4a876902bac1c37850edaf094
bo40 <bo40.git@gmail.com>
2019-06-24 23:08:27 +0900

    fix database query error

--- lib/kumanodocs_hanami/repositories/gijiroku_repository.rb
+++ lib/kumanodocs_hanami/repositories/gijiroku_repository.rb
@@ -1,5 +1,5 @@
 class GijirokuRepository < Hanami::Repository
   def find_latest
-    gijirokus.order { created_at.desc }.limit(1).first
+    gijirokus.order(gijirokus[:created_at].qualified.desc).limit(1).first
   end
 end
