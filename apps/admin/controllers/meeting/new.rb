b38a74a15640cb61cc8cfc80ab1b0fcfdfad1fbe
bo40 <bo40.git@gmail.com>
2019-07-14 15:29:39 +0900

    check admin meeting#new test

--- apps/admin/controllers/meeting/new.rb
+++ apps/admin/controllers/meeting/new.rb
@@ -2,6 +2,10 @@ module Admin::Controllers::Meeting
   class New
     include Admin::Action
 
+    def initialize(authenticator: AdminAuthenticator.new)
+      @authenticator = authenticator
+    end
+
     def call(params)
     end
   end
--- spec/admin/controllers/meeting/new_spec.rb
+++ spec/admin/controllers/meeting/new_spec.rb
@@ -1,11 +1,23 @@
 require_relative '../../../spec_helper'
 
 describe Admin::Controllers::Meeting::New do
-  let(:action) { Admin::Controllers::Meeting::New.new }
+  let(:action) { Admin::Controllers::Meeting::New.new(authenticator: authenticator) }
   let(:params) { Hash[] }
 
-  it 'is successful' do
-    response = action.call(params)
-    response[0].must_equal 200
+  describe 'when user is logged in' do
+    let(:authenticator) { MiniTest::Mock.new.expect(:call, MiniTest::Mock.new.expect(:user, User.new), [nil]) }
+    it 'is successful' do
+      response = action.call(params)
+      response[0].must_equal 200
+    end
+  end
+
+  describe 'when use is not logged in' do
+    let(:authenticator) { MiniTest::Mock.new.expect(:call, MiniTest::Mock.new.expect(:user, nil), [nil])
+                                            .expect(:call, MiniTest::Mock.new.expect(:user, nil), [nil]) }
+    it 'is redirected' do
+      response = action.call(params)
+      response[0].must_equal 302
+    end
   end
 end
