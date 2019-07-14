48a619938917d8f1df8e389240fb9aaa8ce5b027
bo40 <bo40.git@gmail.com>
2019-07-14 15:57:44 +0900

    check admin meeting#edit test

--- apps/admin/controllers/meeting/edit.rb
+++ apps/admin/controllers/meeting/edit.rb
@@ -3,8 +3,10 @@ module Admin::Controllers::Meeting
     include Admin::Action
     expose :meeting
 
-    def initialize(meeting_repo: MeetingRepository.new)
+    def initialize(meeting_repo: MeetingRepository.new,
+                   authenticator: AdminAuthenticator.new)
       @meeting_repo = meeting_repo
+      @authenticator = authenticator
     end
 
     def call(params)
