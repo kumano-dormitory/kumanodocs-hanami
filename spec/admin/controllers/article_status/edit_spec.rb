2cf15abfe45508207ff9381222c065b2d0b3ca79
bo40 <bo40.git@gmail.com>
2019-07-14 23:57:05 +0900

    check admin article_status#edit test

--- spec/admin/controllers/article_status/edit_spec.rb
+++ spec/admin/controllers/article_status/edit_spec.rb
@@ -2,16 +2,32 @@ require 'spec_helper'
 require_relative '../../../../apps/admin/controllers/article_status/edit'
 
 describe Admin::Controllers::ArticleStatus::Edit do
-  let(:action) { Admin::Controllers::ArticleStatus::Edit.new }
-  let(:meeting_repo) { MeetingRepository.new }
-  let(:article) { create(:article) }
+  describe 'when user is logged in' do
+    let(:authenticator) { MiniTest::Mock.new.expect(:call, MiniTest::Mock.new.expect(:user, User.new), [nil]) }
+    let(:meeting) { Meeting.new(id: rand(1..50)) }
+    let(:params) { { id: meeting.id } }
 
-  it 'is successful' do
-    meeting = meeting_repo.find_with_articles(article.meeting_id)
+    it 'is successful' do
+      action = Admin::Controllers::ArticleStatus::Edit.new(
+        meeting_repo: MiniTest::Mock.new.expect(:find_with_articles, meeting, [meeting.id]),
+        authenticator: authenticator,
+      )
+      response = action.call(params)
+      response[0].must_equal 200
+      action.meeting.must_equal meeting
+    end
+  end
 
-    response = action.call(id: article.meeting_id)
-    response[0].must_equal 200
-
-    action.meeting.must_equal meeting
+  describe 'when user is not logged in' do
+    let(:authenticator) { MiniTest::Mock.new.expect(:call, MiniTest::Mock.new.expect(:user, nil), [nil])
+                                            .expect(:call, MiniTest::Mock.new.expect(:user, nil), [nil]) }
+    let(:params) { Hash[] }
+    it 'is redirected' do
+      action = Admin::Controllers::ArticleStatus::Edit.new(
+        meeting_repo: nil, authenticator: authenticator,
+      )
+      response = action.call(params)
+      response[0].must_equal 302
+    end
   end
 end
