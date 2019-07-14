f9e1592dbe97f15f927b2faade08163e36ef9ec1
bo40 <bo40.git@gmail.com>
2019-07-15 00:13:01 +0900

    check admin article_status#update test

--- spec/admin/controllers/article_status/update_spec.rb
+++ spec/admin/controllers/article_status/update_spec.rb
@@ -2,33 +2,64 @@ require 'spec_helper'
 require_relative '../../../../apps/admin/controllers/article_status/update'
 
 describe Admin::Controllers::ArticleStatus::Update do
-  let(:articles_status) {
-    (1..5).map{ |id| {'article_id' => id, 'checked' => [true, nil].sample, 'printed' => [true, nil].sample} }
-  }
-  let(:params) {
-    {
-      meeting: { articles: articles_status },
-      id: rand(1..5)
+  describe 'when user is logged in' do
+    def assert_invalid_params(invalid_params_merged)
+      action = Admin::Controllers::ArticleStatus::Update.new(
+        meeting_repo: MiniTest::Mock.new.expect(:find_with_articles, meeting, [params[:id]]),
+        article_repo: nil, admin_history_repo: nil,
+        authenticator: MiniTest::Mock.new.expect(:call, MiniTest::Mock.new.expect(:user, User.new), [nil]),
+      )
+      invalid_params = params.deep_merge(invalid_params_merged)
+      response = action.call(invalid_params)
+
+      response[0].must_equal 422
+      action.meeting.must_equal meeting
+    end
+
+    let(:meeting) { Meeting.new(id: rand(1..50)) }
+    let(:articles_status) {
+      (1..5).map{ |id| {'article_id' => id, 'checked' => [true, nil].sample} }
     }
-  }
+    let(:params) {
+      {
+        meeting: { articles: articles_status },
+        id: meeting.id,
+      }
+    }
 
-  it 'is successful' do
-    action = Admin::Controllers:: ArticleStatus::Update.new(
-      article_repo: MiniTest::Mock.new.expect(:update_status, nil, [articles_status])
-    )
+    it 'is successful' do
+      article_repo = MiniTest::Mock.new.expect(:update_status, nil, [articles_status])
+      action = Admin::Controllers::ArticleStatus::Update.new(
+        article_repo: article_repo, meeting_repo: nil,
+        admin_history_repo: MiniTest::Mock.new.expect(:add, nil, [:article_status_update, String]),
+        authenticator: MiniTest::Mock.new.expect(:call, MiniTest::Mock.new.expect(:user, User.new), [nil]),
+      )
+      response = action.call(params)
 
-    response = action.call(params)
-    response[0].must_equal 302
+      response[0].must_equal 302
+      article_repo.verify.must_equal true
+    end
+
+    it 'is rejected' do
+      assert_invalid_params({meeting: nil})
+      assert_invalid_params({meeting: rand(-100..100)})
+      assert_invalid_params({meeting: ""})
+      assert_invalid_params({meeting: {articles: nil}})
+      assert_invalid_params({meeting: {articles: {id: rand(1..100)}}})
+      assert_invalid_params({meeting: {articles: "abc"}})
+    end
   end
 
-  it 'is rejected' do
-    meeting = Meeting.new(id: params[:id])
-    action = Admin::Controllers::ArticleStatus::Update.new(
-      meeting_repo: MiniTest::Mock.new.expect(:find_with_articles, meeting, [params[:id]])
-    )
-
-    response = action.call(id: params[:id])
-    response[0].must_equal 422
-    action.meeting.must_equal meeting
+  describe 'when user is not logged in' do
+    let(:authenticator) { MiniTest::Mock.new.expect(:call, MiniTest::Mock.new.expect(:user, nil), [nil])
+                                            .expect(:call, MiniTest::Mock.new.expect(:user, nil), [nil]) }
+    let(:params) { Hash[] }
+    it 'is redirected' do
+      action = Admin::Controllers::ArticleStatus::Update.new(
+        meeting_repo: nil, article_repo: nil, authenticator: authenticator,
+      )
+      response = action.call(params)
+      response[0].must_equal 302
+    end
   end
 end
