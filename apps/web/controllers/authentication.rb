35eedcd1b84006e2cfc024f9792f3fe337678f0a
bo40 <bo40.git@gmail.com>
2019-07-15 14:38:19 +0900

    define JwtAuthenticator Interactor

--- apps/web/controllers/authentication.rb
+++ apps/web/controllers/authentication.rb
@@ -18,18 +18,7 @@ module Web
     end
 
     def authenticated?
-      token = cookies[:token]
-      return false unless token
-
-      rsa_private = OpenSSL::PKey::RSA.new(KUMANODOCS_AUTH_TOKEN_PKEY)
-      rsa_public = rsa_private.public_key
-      begin
-        decoded_token = JWT.decode(token, rsa_public, true, { algorithm: 'RS256' } )
-      rescue => e
-        p e.message
-        return false
-      end
-      decoded_token[0]['version'] == ENV['KUMANODOCS_AUTH_TOKEN_VERSION']
+      !!(@authenticator&.call(cookies[:token]).verification)
     end
   end
 end
