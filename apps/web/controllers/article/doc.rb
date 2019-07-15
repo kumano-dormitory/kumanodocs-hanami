9e415d47afc8782364fda27957ea1ca43a6173fe
bo40 <bo40.git@gmail.com>
2019-07-15 16:59:45 +0900

    check web article#doc test

--- apps/web/controllers/article/doc.rb
+++ apps/web/controllers/article/doc.rb
@@ -9,6 +9,10 @@ module Web
           optional(:type) { filled? & int? & gteq?(0) & lteq?(3) }
         end
 
+        def initialize(authenticator: JwtAuthenticator.new)
+          @authenticator = authenticator
+        end
+
         def call(params)
           # typeは 0: 利用方法, 1: 議案の投稿方法, 2: 議案の編集方法, 3: 表の追加方法
           if params.valid?
