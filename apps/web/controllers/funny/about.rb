265125fc4c8d49742937aad3cf0b80043be91c20
bo40 <bo40.git@gmail.com>
2019-07-16 18:06:06 +0900

    inject JwtAuthenticator

--- apps/web/controllers/funny/about.rb
+++ apps/web/controllers/funny/about.rb
@@ -2,6 +2,10 @@ module Web::Controllers::Funny
   class About
     include Web::Action
 
+    def initialize(authenticator: JwtAuthenticator.new)
+      @authenticator = authenticator
+    end
+
     def call(params)
     end
   end
