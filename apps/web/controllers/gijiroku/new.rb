20ea161376e49b7867c39d38af89a08f80e204f3
bo40 <bo40.git@gmail.com>
2019-07-16 19:14:01 +0900

    check web gijiroku#new test

--- apps/web/controllers/gijiroku/new.rb
+++ apps/web/controllers/gijiroku/new.rb
@@ -2,6 +2,10 @@ module Web::Controllers::Gijiroku
   class New
     include Web::Action
 
+    def initialize(authenticator: JwtAuthenticator.new)
+      @authenticator = authenticator
+    end
+
     def call(params)
     end
   end
