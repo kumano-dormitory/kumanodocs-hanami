af2e946184ecff65045dcd0d318ecf97d9c4a341
bo40 <bo40.git@gmail.com>
2019-07-17 18:23:12 +0900

    modify default tests of web views

--- spec/web/views/article/update_spec.rb
+++ spec/web/views/article/update_spec.rb
@@ -2,15 +2,12 @@ require 'spec_helper'
 require_relative '../../../../apps/web/views/article/update'
 
 describe Web::Views::Article::Update do
-  let(:exposures) { Hash[foo: 'bar'] }
+  let(:exposures) { Hash[format: :html] }
   let(:template)  { Hanami::View::Template.new('apps/web/templates/article/update.html.erb') }
   let(:view)      { Web::Views::Article::Update.new(template, exposures) }
   let(:rendered)  { view.render }
 
-  it 'exposes #foo' do
-    skip 'This is an auto-generated test. Edit it and add your own tests.'
-
-    # Example
-    view.foo.must_equal exposures.fetch(:foo)
+  it 'exposes #format' do
+    view.format.must_equal exposures.fetch(:format)
   end
 end
