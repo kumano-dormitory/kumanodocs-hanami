c667ed7e0f9d71ca4af7a04a51292448913c31e6
bo40 <bo40.git@gmail.com>
2019-07-17 17:59:55 +0900

    check web table#create test

--- apps/web/controllers/table/create.rb
+++ apps/web/controllers/table/create.rb
@@ -16,10 +16,12 @@ module Web::Controllers::Table
 
     def initialize(article_repo: ArticleRepository.new,
                    author_repo: AuthorRepository.new,
-                   table_repo: TableRepository.new)
+                   table_repo: TableRepository.new,
+                   authenticator: JwtAuthenticator.new)
       @article_repo = article_repo
       @author_repo = author_repo
       @table_repo = table_repo
+      @authenticator = authenticator
       @notifications = {}
     end
 
@@ -38,15 +40,19 @@ module Web::Controllers::Table
               redirect_to routes.article_path(id: params[:table][:article_id])
             else
               @notifications = {error: {status: "Authentication Failed:", message: "議案のパスワードが間違っています. 正しいパスワードを入力してください"}}
+              self.status = 401
             end
           rescue CSV::MalformedCSVError
             @notifications = {error: {status: "Error:", message: "入力された表の形式が不正です. 入力をやり直してください"}}
+            self.status = 422
           end
         else
           @notifications = {error: {status: "Error:", message: "表を追加しようとした議案はすでに締め切り日時を過ぎているため編集できません. 必要があれば資料委員会に相談してください"}}
+          self.status = 422
         end
       else
         @notifications = {error: {status: "Error:", message: "入力された項目に不備があります. もう一度確認してください"}}
+        self.status = 422
       end
 
       if after_deadline?
@@ -56,7 +62,6 @@ module Web::Controllers::Table
         # 締め切り前なので、締切を過ぎていない議案全てに表が投稿できる
         @articles = @article_repo.before_deadline
       end
-      self.status = 422
     end
 
     def notifications
