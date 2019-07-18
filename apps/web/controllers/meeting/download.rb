595c674726126d64e8cedca125e50da88ac3bc4f
bo40 <bo40.git@gmail.com>
2019-07-18 15:19:20 +0900

    implementation of GeneratePdf interactor

--- apps/web/controllers/meeting/download.rb
+++ apps/web/controllers/meeting/download.rb
@@ -11,43 +11,24 @@ module Web::Controllers::Meeting
     end
 
     def initialize(meeting_repo: MeetingRepository.new,
-                   article_repo: ArticleRepository.new,
-                   comment_repo: CommentRepository.new,
+                   generate_pdf_interactor: GeneratePdf.new,
                    authenticator: JwtAuthenticator.new)
       @meeting_repo = meeting_repo
-      @article_repo = article_repo
-      @comment_repo = comment_repo
+      @generate_pdf_interactor = generate_pdf_interactor
       @authenticator = authenticator
     end
 
     def call(params)
       if params.valid?
         # ブロック会議が指定されているのでPDFを返す
-        @meeting = @meeting_repo.find_with_articles(params[:id])
-
-        @articles = @meeting.articles.map{ |article| @article_repo.find_with_relations(article.id) }
-        past_meeting = @meeting_repo.find_past_meeting(@meeting.id)
-        @past_comments = @comment_repo.by_meeting(past_meeting.id)
-                                      .group_by{|comment| comment[:article_id]}
-        @tex_str = Web::Views::Meeting::Download.render(
-          format: :tex, meeting: @meeting, articles: @articles, past_comments: @past_comments
-        )
-        digest = Digest::MD5.hexdigest(@tex_str)
-        tmp_filename = "kumanodocs_meeting_#{@meeting.id}"
-        tmp_folderpath = "/tmp/kumanodocs/meeting#{@meeting.id}/a#{digest}/"
-
-        unless FileTest.exist?("#{tmp_folderpath}#{tmp_filename}.pdf")
-          FileUtils.mkdir_p(tmp_folderpath) unless FileTest.exist?(tmp_folderpath)
-          open("#{tmp_folderpath}#{tmp_filename}.tex", "w") do |f|
-            f.puts(@tex_str)
-          end
-          halt 500 unless IO.popen("ptex2pdf -u -l -output-directory #{tmp_folderpath} #{tmp_folderpath}#{tmp_filename}.tex") { |io| $? == nil || $? == 0 }
-          halt 500 unless IO.popen("ptex2pdf -u -l -output-directory #{tmp_folderpath} #{tmp_folderpath}#{tmp_filename}.tex") { |io| $? == 0 }
-        end
+        @meeting = @meeting_repo.find(params[:id])
+        specification = Specifications::Pdf.new(type: :web_articles, meeting_id: @meeting.id)
+        result = @generate_pdf_interactor.call(specification)
+        halt 500 if result.failure?
 
         self.format = :pdf
         self.headers.merge!({'Content-Disposition' => "attachment; filename=\"kumanodocs_meeting_#{@meeting.date}.pdf\""})
-        unsafe_send_file Pathname.new("#{tmp_folderpath}#{tmp_filename}.pdf")
+        unsafe_send_file Pathname.new(result.path)
       else
         # ブロック会議を指定する画面を表示する
         @meetings = @meeting_repo.desc_by_date(limit: 20)
