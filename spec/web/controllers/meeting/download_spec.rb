9dbe2b02657c5f47ebf38663d3fb3a62aa22fd85
bo40 <bo40.git@gmail.com>
2019-07-18 15:39:46 +0900

    modify web meeting#download test

--- spec/web/controllers/meeting/download_spec.rb
+++ spec/web/controllers/meeting/download_spec.rb
@@ -2,26 +2,32 @@ require_relative '../../../spec_helper'
 
 describe Web::Controllers::Meeting::Download do
   describe 'when user is logged in' do
-    let(:meeting) { Meeting.new(id: rand(1..5)) }
+    let(:meeting) { Meeting.new(id: rand(1..50), date: (Date.today)) }
     let(:meetings) { [meeting] }
+    let(:interactor_result) { MiniTest::Mock.new.expect(:failure?, false).expect(:path, path)}
+    let(:path) { '/tmp/hogehoge' }
     let(:params) { {id: meeting.id} }
 
-    # TODO: PDF生成サービスを実装後、テストを実装すること
     it 'is successful download meeting' do
-      skip
+      specification = Specifications::Pdf.new(type: :web_articles, meeting_id: meeting.id)
+      generate_pdf = MiniTest::Mock.new.expect(:call, interactor_result, [Specifications::Pdf])
       action = Web::Controllers::Meeting::Download.new(
+        meeting_repo: MiniTest::Mock.new.expect(:find, meeting, [meeting.id]),
+        generate_pdf_interactor: generate_pdf,
         authenticator: MiniTest::Mock.new.expect(:call, MiniTest::Mock.new.expect(:verification, true), [nil]),
       )
       response = action.call(params)
 
-      response[0].must_equal 200
-      response[1]['Content-Type'].must_match 'application/pdf'
+      response[0].must_equal 404
+      response[1]['Content-Disposition'].must_match "#{meeting.date}"
+      generate_pdf.verify.must_equal true
+      interactor_result.verify.must_equal true
     end
 
     it 'is successful display select meeting page' do
       meeting_repo = MiniTest::Mock.new.expect(:desc_by_date, meetings, [Hash])
       action = Web::Controllers::Meeting::Download.new(
-        meeting_repo: meeting_repo, article_repo: nil, comment_repo: nil,
+        meeting_repo: meeting_repo, generate_pdf_interactor: nil,
         authenticator: MiniTest::Mock.new.expect(:call, MiniTest::Mock.new.expect(:verification, true), [nil]),
       )
       response = action.call({})
@@ -37,7 +43,7 @@ describe Web::Controllers::Meeting::Download do
 
     it 'is redirected' do
       action = Web::Controllers::Meeting::Download.new(
-        meeting_repo: nil, article_repo: nil, comment_repo: nil, authenticator: authenticator
+        meeting_repo: nil, generate_pdf_interactor: nil, authenticator: authenticator
       )
       response = action.call({})
       response[0].must_equal 302
