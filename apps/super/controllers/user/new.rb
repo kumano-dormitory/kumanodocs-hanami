466f575909af6189e37cf5b3c5ced2e62bc3570e
bo40 <bo40.git@gmail.com>
2019-10-04 17:27:03 +0900

    implement super user actions

---
+++ apps/super/controllers/user/new.rb
@@ -0,0 +1,11 @@
+module Super::Controllers::User
+  class New
+    include Super::Action
+
+    def initialize()
+    end
+
+    def call(params)
+    end
+  end
+end
---
+++ apps/super/views/user/top.rb
@@ -0,0 +1,9 @@
+module Super
+  module Views
+    module User
+      class Top
+        include Super::View
+      end
+    end
+  end
+end
