72dee70c9638f5709477ab5bd71799293ef34a88
bo40 <bo40.git@gmail.com>
2019-10-05 11:12:12 +0900

    implement database access to super application

--- apps/super/config/routes.rb
+++ apps/super/config/routes.rb
@@ -6,3 +6,5 @@ resources :session, only: [:new, :create, :destroy]
 get '/user/top', to: 'user#top'
 get '/logout', to: 'session#destroy', as: :logout
 get '/log', to: 'log#index', as: :logs
+get '/db', to: 'db#index', as: :dbs
+post '/db', to: 'db#run', as: :dbs
