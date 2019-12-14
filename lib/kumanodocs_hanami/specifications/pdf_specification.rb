d2b6af713e5496e71177d2e90774bc3763f3ba79
bo40 <bo40.git@gmail.com>
2019-12-14 23:16:36 +0900

    表投稿時にlatexコンパイル処理に通るかチェックを行う

--- lib/kumanodocs_hanami/specifications/pdf_specification.rb
+++ lib/kumanodocs_hanami/specifications/pdf_specification.rb
@@ -1,15 +1,17 @@
 module Specifications
   class Pdf
-    attr_reader :type, :meeting_id, :after_6pm, :article_id
+    attr_reader :type, :meeting_id, :after_6pm, :article_id, :data
 
     def initialize(type: :web_articles,
                    meeting_id: 1,
                    after_6pm: false,
-                   article_id: 0)
+                   article_id: 0,
+                   data: {})
       @type = type
       @meeting_id = meeting_id
       @after_6pm = after_6pm
       @article_id = article_id
+      @data = data
     end
 
     def ==(other)
