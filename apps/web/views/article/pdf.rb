8ff8aa2974b27af3024e719362fd4b61dee3640e
bo40 <bo40.git@gmail.com>
2019-12-14 20:02:33 +0900

    implement web article#pdf action

---
+++ apps/web/views/article/pdf.rb
@@ -0,0 +1,5 @@
+module Web::Views::Article
+  class Pdf
+    include Web::View
+  end
+end
--- lib/kumanodocs_hanami/specifications/pdf_specification.rb
+++ lib/kumanodocs_hanami/specifications/pdf_specification.rb
@@ -1,13 +1,15 @@
 module Specifications
   class Pdf
-    attr_reader :type, :meeting_id, :after_6pm
+    attr_reader :type, :meeting_id, :after_6pm, :article_id
 
     def initialize(type: :web_articles,
                    meeting_id: 1,
-                   after_6pm: false)
+                   after_6pm: false,
+                   article_id: 0)
       @type = type
       @meeting_id = meeting_id
       @after_6pm = after_6pm
+      @article_id = article_id
     end
 
     def ==(other)
@@ -15,7 +17,7 @@ module Specifications
     end
 
     def eql?(other)
-      @type == other.type && @meeting_id == other.meeting_id && @after_6pm == other.after_6pm
+      @type == other.type && @meeting_id == other.meeting_id && @after_6pm == other.after_6pm && @article_id == other.article_id
     end
   end
 end
