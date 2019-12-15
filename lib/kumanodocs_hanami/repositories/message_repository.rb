38c4689f3eadbfd5a0edfd9dae55922032a1a09a
bo40 <bo40.git@gmail.com>
2019-12-15 16:19:07 +0900

    前回のブロック会議からに議事録への返答を表示

--- lib/kumanodocs_hanami/repositories/message_repository.rb
+++ lib/kumanodocs_hanami/repositories/message_repository.rb
@@ -11,4 +11,12 @@ class MessageRepository < Hanami::Repository
       .order(:created_at)
       .to_a
   end
+
+  def by_meeting(id)
+    query = "\
+    SELECT messages.id, messages.body, messages.send_by_article_author, articles.id as article_id, articles.number as article_number, comments.id as comment_id \
+    FROM messages JOIN comments ON (messages.comment_id = comments.id) JOIN articles ON (comments.article_id = articles.id) \
+    WHERE (articles.meeting_id = #{id}) ORDER BY article_number nulls last, article_id, messages.id"
+    messages.read(query).map.to_a
+  end
 end
