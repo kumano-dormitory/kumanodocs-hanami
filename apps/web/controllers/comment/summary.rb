80250ddf4e88db09c233216d7c28749fc7303042
bo40 <bo40.git@gmail.com>
2019-12-17 17:25:37 +0900

    代議員会用の議事録まとめ閲覧ページの実装

---
+++ apps/web/controllers/comment/summary.rb
@@ -0,0 +1,35 @@
+module Web::Controllers::Comment
+  class Summary
+    include Web::Action
+    expose :meetings, :meeting, :comments, :messages, :max_page, :page
+
+    def initialize(meeting_repo: MeetingRepository.new,
+                   comment_repo: CommentRepository.new,
+                   message_repo: MessageRepository.new,
+                   authenticator: JwtAuthenticator.new)
+      @meeting_repo = meeting_repo
+      @comment_repo = comment_repo
+      @message_repo = message_repo
+      @authenticator = authenticator
+    end
+
+    def call(params)
+      @meetings = @meeting_repo.desc_by_date(limit: 15).filter{ |m| m.date > Date.new(2019,6,22) }
+
+      @page = params[:page]&.to_i || 0
+      @page = 0 if @page < 0
+      @page = (@meetings.length - 1) if @page > (@meetings.length - 1)
+
+      @meeting = @meetings[@page]
+
+      # ブロック会議の０番（前回のブロック会議から）
+      @comments = @comment_repo.by_meeting(@meeting.id)
+                                    .group_by{|comment| comment[:article_id]}
+      @messages = @message_repo.by_meeting(@meeting.id)
+                                    .group_by{|message| message[:comment_id]}
+
+
+      @max_page = @meetings.length - 1
+    end
+  end
+end
