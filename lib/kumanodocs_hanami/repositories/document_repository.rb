d42e41c40ae7780632afe167d74d79d4ee361957
bo40 <bo40.git@gmail.com>
2020-03-07 17:08:39 +0900

    DocumentRepositoryの実装, Documentのシード値の実装

--- lib/kumanodocs_hanami/repositories/document_repository.rb
+++ lib/kumanodocs_hanami/repositories/document_repository.rb
@@ -1,2 +1,25 @@
 class DocumentRepository < Hanami::Repository
+  associations do
+    belongs_to :user
+  end
+
+  def order_by_number
+    aggregate(:user)
+      .documents
+      .order(documents[:number].asc(nulls: :last), documents[:id].asc)
+  end
+
+  def find_with_relations(id)
+    aggregate(:user)
+      .documents
+      .where(id: id)
+      .map_to(Document)
+      .one
+  end
+
+  def by_user(user_id)
+    documents
+      .where(user_id: user_id)
+      .order(documents[:number].asc(nulls: :last), documents[:id].asc)
+  end
 end
