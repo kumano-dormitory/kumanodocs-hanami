9db528f80837a2d56dd67217b5aff141fe8475ec
bo40 <bo40.git@gmail.com>
2020-03-10 09:28:33 +0900

    資料投稿時に資料の種別を選択できるようにコントローラの修正

--- apps/web/controllers/docs/create.rb
+++ apps/web/controllers/docs/create.rb
@@ -8,7 +8,9 @@ module Web::Controllers::Docs
         required(:title).filled(:str?)
         required(:user_id).filled(:str?)
         required(:type) { filled? & int? & gteq?(0) & lt?(3) }
-        required(:body).filled(:str?)
+        optional(:body).maybe(:str?)
+        optional(:data) { filled? }
+        optional(:url).maybe(:str?)
       end
     end
 
@@ -25,13 +27,38 @@ module Web::Controllers::Docs
       @user = @user_repo.find(session[:user_id])
       if !@user.nil? && @user.authority == 1
         if params.valid?
-          props = {
-            title: params[:document][:title],
-            user_id: @user.id,
-            type: params[:document][:type],
-            body: params[:document][:body]
-          }
-          document = @document_repo.create(props)
+          case params[:document][:type]
+          when 0 then
+            props = {
+              title: params[:document][:title],
+              user_id: @user.id,
+              type: params[:document][:type],
+              body: params[:document][:body]
+            }
+            document = @document_repo.create(props)
+          when 1 then
+            props = {
+              title: params[:document][:title],
+              user_id: @user.id,
+              type: params[:document][:type],
+              body: params[:document][:data][:filename]
+            }
+            document = @document_repo.create(props)
+
+            folder_path = "./docs/id#{document.id}/"
+            FileUtils.mkdir_p(folder_path) unless FileTest.exist?(folder_path)
+            FileUtils.mv(params[:document][:data][:tempfile], "#{folder_path}#{params[:document][:data][:filename]}")
+          when 2 then
+            props = {
+              title: params[:document][:title],
+              user_id: @user.id,
+              type: params[:document][:type],
+              body: params[:document][:url]
+            }
+            document = @document_repo.create(props)
+          else
+            halt 422
+          end
           redirect_to routes.document_path(document.id)
         else
           @notifications = {error: {status: "Error:", message: "入力された項目に不備があり新規追加できません. もう一度確認してください"}}
