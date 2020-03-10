3fbeb918dc00def01db9b6e2faa3f2670190b3b9
bo40 <bo40.git@gmail.com>
2020-03-10 10:25:24 +0900

    modify require path

--- spec/web/views/docs/login/create_spec.rb
+++ spec/web/views/docs/login/create_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../../../spec_helper'
+require_relative '../../../../spec_helper'
 
 describe Web::Views::Docs::Login::Create do
   let(:exposures) { Hash[format: :html] }
--- spec/web/views/docs/login/new_spec.rb
+++ spec/web/views/docs/login/new_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../../../spec_helper'
+require_relative '../../../../spec_helper'
 
 describe Web::Views::Docs::Login::New do
   let(:exposures) { Hash[format: :html] }
