b91ad732bde7bfcd20b7b20a108c48035b678b93
bo40 <bo40.git@gmail.com>
2020-03-10 17:41:23 +0900

    Web docs/login#createのテスト実装

--- spec/web/controllers/docs/login/create_spec.rb
+++ spec/web/controllers/docs/login/create_spec.rb
@@ -1,11 +1,50 @@
 require_relative '../../../../spec_helper'
 
 describe Web::Controllers::Docs::Login::Create do
-  let(:action) { Web::Controllers::Docs::Login::Create.new }
-  let(:params) { Hash[] }
+  describe 'when user is logged in' do
+    let(:authenticator) { MiniTest::Mock.new.expect(:call, MiniTest::Mock.new.expect(:verification, true), [nil]) }
+    let(:password) { Faker::Internet.password }
+    let(:wrong_password) { Faker::Internet.password }
+    let(:user) { User.new(id: rand(1..10), authority: 1, crypt_password: BCrypt::Password.create(password), name: Faker::Name.name) }
+    let(:params) {{
+      user: {name: user.name, password: password}
+    }}
+    let(:invalid_params) {{
+      user: {name: user.name, password: wrong_password}
+    }}
 
-  it 'is successful' do
-    response = action.call(params)
-    response[0].must_equal 200
+    it 'is successful login' do
+      user_repo = MiniTest::Mock.new.expect(:find_by_name, user, [user.name])
+      action = Web::Controllers::Docs::Login::Create.new(
+        user_repo: user_repo, authenticator: authenticator
+      )
+      response = action.call(params)
+      response[0].must_equal 302
+      user_repo.verify.must_equal true
+    end
+
+    it 'is rejected by wrong password' do
+      user_repo = MiniTest::Mock.new.expect(:find_by_name, user, [user.name])
+      action = Web::Controllers::Docs::Login::Create.new(
+        user_repo: user_repo, authenticator: authenticator
+      )
+      response = action.call(invalid_params)
+      response[0].must_equal 403
+      user_repo.verify.must_equal true
+    end
+  end
+
+  describe 'when user is not logged in' do
+    let(:authenticator) { MiniTest::Mock.new.expect(:call, MiniTest::Mock.new.expect(:verification, false), [nil]) }
+    let(:action) {
+      Web::Controllers::Docs::Login::Create.new(
+        user_repo: nil, authenticator: authenticator
+      )
+    }
+
+    it 'is redirected' do
+      response = action.call({})
+      response[0].must_equal 302
+    end
   end
 end
