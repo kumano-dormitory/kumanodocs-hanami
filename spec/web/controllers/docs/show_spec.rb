ea4b73cd6f1328e1464c370a022c33ba46198966
bo40 <bo40.git@gmail.com>
2020-03-10 17:34:47 +0900

    Web docs#showのテスト実装

--- spec/web/controllers/docs/show_spec.rb
+++ spec/web/controllers/docs/show_spec.rb
@@ -1,11 +1,34 @@
 require_relative '../../../spec_helper'
 
 describe Web::Controllers::Docs::Show do
-  let(:action) { Web::Controllers::Docs::Show.new }
-  let(:params) { Hash[] }
+  describe 'when user is logged in' do
+    let(:authenticator) { MiniTest::Mock.new.expect(:call, MiniTest::Mock.new.expect(:verification, true), [nil]) }
+    let(:document_repo) { MiniTest::Mock.new.expect(:find_with_relations, document, [id]) }
+    let(:document) { Document.new(id: id) }
+    let(:id) { rand(1..10) }
+    let(:params) { {id: id} }
 
-  it 'is successful' do
-    response = action.call(params)
-    response[0].must_equal 200
+    it 'is successful' do
+      action = Web::Controllers::Docs::Show.new(
+        document_repo: document_repo, authenticator: authenticator
+      )
+      response = action.call(params)
+      response[0].must_equal 200
+      action.document.must_equal document
+    end
+  end
+
+  describe 'when user is not logged in' do
+    let(:authenticator) { MiniTest::Mock.new.expect(:call, MiniTest::Mock.new.expect(:verification, false), [nil]) }
+    let(:action) {
+      Web::Controllers::Docs::Show.new(
+        document_repo: nil, authenticator: authenticator
+      )
+    }
+
+    it 'is redirected' do
+      response = action.call({})
+      response[0].must_equal 302
+    end
   end
 end
