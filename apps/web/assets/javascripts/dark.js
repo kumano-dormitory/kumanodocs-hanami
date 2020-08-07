5387eaf5998efa6f3748435161df27a682d8245d
bo40 <bo40.git@gmail.com>
2020-08-07 19:14:55 +0900

    dark modeの実装

---
+++ apps/web/assets/javascripts/dark.js
@@ -0,0 +1,50 @@
+function setDark() {
+  document.body.classList.add('is-dark');
+
+  var header = document.getElementById('navigation');
+  if (header) { header.classList.add('is-dark'); }
+  var topimg = document.getElementById('top-img');
+  if (topimg) {
+    topimg.src = "/assets/abstruct-dark.svg";
+  }
+  var forms = document.getElementsByTagName('form');
+  for (var i = 0; i < forms.length; i++) {
+    forms[i].classList.add('is-dark');
+  }
+}
+
+function setLight() {
+  document.body.classList.remove('is-dark');
+  var header = document.getElementById('navigation');
+  if (header) { header.classList.remove('is-dark'); }
+  var topimg = document.getElementById('top-img');
+  if (topimg) {
+    topimg.src = "/assets/abstruct.svg";
+  }
+  var forms = document.getElementsByTagName('form');
+  for (var i = 0; i < forms.length; i++) {
+    forms[i].classList.remove('is-dark');
+  }
+}
+
+function initialize() {
+  const localTheme = localStorage.getItem('theme-mode');
+  if (localTheme == 'dark') {
+    setDark();
+  }
+  const button = document.getElementById("theme-mode-btn");
+  if (button) {
+    button.checked = (localTheme == 'dark');
+    button.addEventListener('change', function(e) {
+      if (e.target.checked) {
+        localStorage.setItem('theme-mode','dark');
+        setDark();
+      } else {
+        localStorage.setItem('theme-mode','light');
+        setLight();
+      }
+    });
+  }
+}
+
+initialize();
