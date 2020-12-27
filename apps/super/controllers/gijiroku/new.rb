4bcc823629df9df2fc47c15da09e4045c04029be
bo40 <bo40.git@gmail.com>
2020-12-28 02:33:57 +0900

    implement super gijiroku#new

---
+++ apps/super/controllers/gijiroku/new.rb
@@ -0,0 +1,13 @@
+# ====
+# 寮生大会議事録の新規作成ページ表示アクション
+# ====
+# 寮生大会議事録を新規作成するページを表示する
+
+module Super::Controllers::Gijiroku
+  class New
+    include Super::Action
+
+    def call(params)
+    end
+  end
+end
---
+++ apps/super/views/gijiroku/new.rb
@@ -0,0 +1,5 @@
+module Super::Views::Gijiroku
+  class New
+    include Super::View
+  end
+end
