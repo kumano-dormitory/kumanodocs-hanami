20c60c4274250b9b2769f68f5f5c82fb0c938497
bo40 <bo40.git@gmail.com>
2020-12-28 02:30:37 +0900

    add gijiroku routes to super

--- lib/kumanodocs_hanami/repositories/gijiroku_repository.rb
+++ lib/kumanodocs_hanami/repositories/gijiroku_repository.rb
@@ -2,4 +2,8 @@ class GijirokuRepository < Hanami::Repository
   def find_latest
     gijirokus.order(gijirokus[:created_at].qualified.desc).limit(1).first
   end
+
+  def desc_by_created_at
+    gijirokus.order {created_at.desc}
+  end
 end
