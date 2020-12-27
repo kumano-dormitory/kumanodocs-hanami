2aa9ec77730e716ce118b0e6e40bfb3603a8391c
bo40 <bo40.git@gmail.com>
2020-12-28 02:30:37 +0900

    add gijiroku routes to super

--- apps/super/config/routes.rb
+++ apps/super/config/routes.rb
@@ -2,9 +2,11 @@ root to: 'user#top'
 
 resources :user
 resources :session, only: [:new, :create, :destroy]
+resources :gijiroku
 
 get '/user/top', to: 'user#top'
 get '/logout', to: 'session#destroy', as: :logout
 get '/log', to: 'log#index', as: :logs
 get '/db', to: 'db#index', as: :dbs
 post '/db', to: 'db#run', as: :dbs
+get '/gijiroku/:id/destroy', to: 'gijiroku#destroy', as: :destroy_gijiroku
