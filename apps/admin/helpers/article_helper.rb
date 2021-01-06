14957c442c5efe56f4e01bce0f0f3bb0483a41ca
bo40 <bo40.git@gmail.com>
2021-01-06 21:07:10 +0900

    bugfix admin article_title helper

--- apps/admin/helpers/article_helper.rb
+++ apps/admin/helpers/article_helper.rb
@@ -4,7 +4,7 @@ module Admin
   module Helpers
     module ArticleHelper
       def article_numbered_title(article)
-         "(#{article.number.nil? ? '番号なし' : article.number}) #{h article.title}"
+         "(#{article.number.nil? ? '番号なし' : article.number}) #{article.title}"
       end
 
       def article_formatted_title(article, checked: false, number: true)
@@ -12,7 +12,7 @@ module Admin
         categories_str = article&.categories&.map{ |category| "#{category.name}"}&.reduce{|ret, str| "#{ret}・#{str}"}
         if categories_str then categories_str = "【#{categories_str}】" end
         checked_str = if checked then (article.checked ? '' : '【追加議案】') else '' end
-        "#{number_str}#{checked_str}#{h article.title} #{categories_str}"
+        "#{number_str}#{checked_str}#{article.title} #{categories_str}"
       end
 
       def vote_content(article)
