e35e3b772f9b8cbb8929a760ba9fc4f1ce9cdc17
bo40 <bo40.git@gmail.com>
2021-01-08 21:57:09 +0900

    議案一覧をdrawerで表示するように修正

--- apps/web/assets/javascripts/contextual_menu.js
+++ apps/web/assets/javascripts/contextual_menu.js
@@ -1,44 +1,51 @@
-function toggleMenu(element, show) {
-  element.setAttribute('aria-expanded', show);
-  document
-    .querySelector(element.getAttribute('aria-controls'))
-    .setAttribute('aria-hidden', !show);
+function toggleDrawer(sideNavigation, show) {
+  if (sideNavigation) {
+    if (show) {
+      sideNavigation.classList.remove('is-collapsed');
+      sideNavigation.classList.add('is-expanded');
+    } else {
+      sideNavigation.classList.remove('is-expanded');
+      sideNavigation.classList.add('is-collapsed');
+    }
+  }
 }
 
-function setupContextualMenuListeners(contextualMenuToggleSelector) {
-  const toggles = document.querySelectorAll(contextualMenuToggleSelector);
+function setupSideNavigation(sideNavigation) {
+  var toggles = [].slice.call(sideNavigation.querySelectorAll('.js-drawer-toggle'));
+
+  toggles.forEach(function (toggle) {
+    toggle.addEventListener('click', function (event) {
+      event.preventDefault();
+      var sideNav = document.getElementById(toggle.getAttribute('aria-controls'));
+
+      if (sideNav) {
+        toggleDrawer(sideNav, !sideNav.classList.contains('is-expanded'));
+      }
+    });
+  });
+}
+
+function setupSideNavigations(sideNavigationSelector) {
+  var sideNavigations = [].slice.call(document.querySelectorAll(sideNavigationSelector));
+
+  sideNavigations.forEach(setupSideNavigation);
+}
+
+setupSideNavigations('.p-side-navigation, [class*="p-side-navigation--"]');
+
+function setupTopDrawerListeners(sideNavigationToggleSelector) {
+  const toggles = document.querySelectorAll(sideNavigationToggleSelector);
 
   toggles.forEach(toggle => {
     toggle.addEventListener('click', e => {
       e.preventDefault();
+      var sideNav = document.getElementById(toggle.getAttribute('aria-controls'));
 
-      const target = e.target;
-      const menuAlreadyOpen = target.getAttribute('aria-expanded') === 'true';
-
-      toggleMenu(target, !menuAlreadyOpen)
+      if (sideNav) {
+        toggleDrawer(sideNav, !sideNav.classList.contains('is-expanded'));
+      }
     });
   });
-
-  document.addEventListener('click', e => {
-    toggles.forEach(toggle => {
-      const contextualMenu = document.querySelector(toggle.getAttribute('aria-controls'));
-      const clickOutside = !(toggle.contains(e.target) || contextualMenu.contains(e.target));
-
-      if (clickOutside) {
-        toggleMenu(toggle, false);
-      }
-    })
-  });
-
-  document.onkeydown = e => {
-    e = e || window.event;
-
-    if (e.keyCode === 27) {
-      toggles.forEach(toggle => {
-        toggleMenu(toggle, false);
-      });
-    }
-  };
 }
 
-setupContextualMenuListeners('.p-contextual-menu__toggle');
+setupTopDrawerListeners('#side-navigation-top-drawer');
