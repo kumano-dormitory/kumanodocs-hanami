9ca5ef5d3785e40a9422802ee3439f796108ce15
bo40 <bo40.git@gmail.com>
2021-02-12 10:04:43 +0900

    implement apiv2 article#index

--- apps/apiv2/config/routes.rb
+++ apps/apiv2/config/routes.rb
@@ -1,5 +1,2 @@
-# Configure your routes here
-# See: https://guides.hanamirb.org/routing/overview
-#
-# Example:
-# get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
+resources :meetings, only: [:index, :show]
+resources :articles, only: [:index, :show]
