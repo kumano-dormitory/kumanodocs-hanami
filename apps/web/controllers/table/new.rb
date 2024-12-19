27eeb277a5a7cfbb9cbeed63db8fc32242d136b5
ShimadaYusei <yuseishimada999@gmail.com>
2024-12-19 19:50:16 +0900

    fix not_checked_for_next_meeting at article?repository

--- apps/web/controllers/table/new.rb
+++ apps/web/controllers/table/new.rb
@@ -18,7 +18,13 @@ module Web::Controllers::Table
       if params[:article_id]
         @article_id = params[:article_id].to_i
       end
-      @articles = @article_repo.before_deadline
+      if after_deadline?
+        # 締め切り後なので追加議案にのみ表が追加できる
+        @articles = @article_repo.not_checked_for_next_meeting
+      else
+        # 締め切り前なので、締切を過ぎていない議案全てに表が投稿できる
+        @articles = @article_repo.before_deadline
+      end
     end
   end
 end
